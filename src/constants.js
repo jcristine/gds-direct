bf99e382ac391c885681ec8ac534f5232c74e283
Arturs Osipovs <a.osipov@dyninno.lv>
2016-11-01 16:38:32 +0200

    sabre

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const TERMINAL_HEIGHT 		= '650';
 export const TERMINAL_SPLIT_HEIGHT 	= '325';
 
-export const END_POINT_URL	 		= '?id=sabre/terminal&her=1';
+export const END_POINT_URL	 		= '../?id=sabre/terminal&her=1';
 export const TIME_FORMAT 			= '12';
 export const ACCOUNT 				= 'training';
