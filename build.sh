f277d69ef5e6c581469dc5ca8b235519de061388
Arturs Klesuns <a.klesuns@dynatech.lv>
2018-12-03 13:48:40 +0000

    Add generated CSS file under version control

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # less should be installed globally - npm install less -g
-lessc public/_style/less/main.less > public/_style/css/parsed.css
+# lessc public/_style/less/main.less > public/_style/css/parsed.css
 
 # downloads /node_modules/ if needed
 npm install --from-lock-file
