28e8ec196c94b1ebfab2b81bed83260815d18337
Arturs Klesuns <a.klesuns@dynatech.lv>
2018-12-11 15:39:22 +0000

    Nah, screw all this - just show last commit in the runner

---
+++ tests/runTests.js
@@ -0,0 +1,9 @@
+
+console.log('Starting unit tests');
+
+if (3 > 4) {
+	console.error('Your code says that 3 > 4, this is wrong');
+}
+if (5 > 4) {
+	console.error('A broken test failed asserting that 5 > 4 is false');
+}
