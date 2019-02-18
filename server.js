aab19bcb433c38c40edf2a25087d43117c976bcc
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-02-18 20:02:47 +0000

    Add test http controller
    To test it without DB and Redis since they seem to be absent from config lan

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 
-console.log('starting server.js');
+let Diag = require('./LibWrappers/Diag.js');
+Diag.error('test diag message', {please: 'work'});
 
-const Config = require('./backend/Config.js');
-(async () => {
-	await Config.fetchExternalConfig();
+/** @debug */
+let express = require('express');
+let app = express();
+app.get('/test123', (req, res) => {
+	res.send(JSON.stringify({test: 'success yay ololo'}));
+});
 
-	require('./backend/WebRoutes.js');
-})();
+app.listen(3011, function () {
+	console.log('listening on *:' + 3011 + ' - for standard http request handling');
+});
+
+// const Config = require('./backend/Config.js');
+// (async () => {
+// 	await Config.fetchExternalConfig();
+//
+// 	require('./backend/WebRoutes.js');
+// })();
