ec8ce488d14989ba2cc0c38886445b8cb3195216
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-02-19 14:16:45 +0000

    Uncomment real listener, lan config should work now

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
 
 const Config = require('./backend/Config.js');
+(async () => {
+	await Config.fetchExternalConfig();
 
-let Diag = require('./backend/LibWrappers/Diag.js');
-Diag.error('test diag message', {please: 'work'});
-
-/** @debug */
-let express = require('express');
-let app = express();
-app.get('/test123', (req, res) => {
-	res.send(JSON.stringify({test: 'success yay ololo'}));
-});
-app.get('/testConfigLan', (req, res) => {
-	Config.fetchExternalConfig()
-		.then(none => res.send(JSON.stringify({message: 'success'})))
-		.catch(exc => res.send('Failed to fetch config lan - ' + exc));
-});
-
-app.listen(process.env.HTTP_PORT, process.env.HOST, function () {
-	console.log('listening on *:' + process.env.HTTP_PORT + ' - for standard http request handling');
-});
-
-// const Config = require('./backend/Config.js');
-// (async () => {
-// 	await Config.fetchExternalConfig();
-//
-// 	require('./backend/WebRoutes.js');
-// })();
+	require('./backend/WebRoutes.js');
+})();
