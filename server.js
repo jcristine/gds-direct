fbda1376bfe489874c5c5be4c3dab679f05c7b08
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-02-19 13:30:58 +0000

    Add an API to test lan config

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 
+const Config = require('./backend/Config.js');
+
 let Diag = require('./backend/LibWrappers/Diag.js');
 Diag.error('test diag message', {please: 'work'});
 
@@ -7,6 +9,11 @@ let express = require('express');
 let app = express();
 app.get('/test123', (req, res) => {
 	res.send(JSON.stringify({test: 'success yay ololo'}));
+});
+app.get('/testConfigLan', (req, res) => {
+	Config.fetchExternalConfig()
+		.then(none => res.send(JSON.stringify({message: 'success'})))
+		.catch(exc => res.send('Failed to fetch config lan - ' + exc));
 });
 
 app.listen(process.env.HTTP_PORT, process.env.HOST, function () {
