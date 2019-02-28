bb648009eeaa90f39693aca44b8f0166692067d5
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-02-28 21:24:03 +0000

    Enable everything back, RBS said the "405 Not Allowed" was fixed by admins, so I guess it is not related to me

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 
 const Config = require('./backend/Config.js');
 (async () => {
-	// console.log('fetching external config');
-	// await Config.fetchExternalConfig();
-	// console.log('initializing Web Routes');
-	// require('./backend/WebRoutes.js');
-	// let Diag = require('./backend/LibWrappers/Diag.js');
-	// let Migration = require('./backend/Migration.js');
-	// Migration.run()
-	// 	.then(result => Diag.notice('Migration was successful', result))
-	// 	.catch(exc => {
-	// 		let msg = 'Migration failed';
-	// 		let data = {
-	// 			httpStatusCode: exc.httpStatusCode,
-	// 			message: exc + '',
-	// 			stack: exc.stack,
-	// 		};
-	// 		console.error(msg, data);
-	// 		return Diag.critical(msg, data);
-	// 	});
+	console.log('fetching external config');
+	await Config.fetchExternalConfig();
+	console.log('initializing Web Routes');
+	require('./backend/WebRoutes.js');
+	let Diag = require('./backend/LibWrappers/Diag.js');
+	let Migration = require('./backend/Migration.js');
+	Migration.run()
+		.then(result => Diag.notice('Migration was successful', result))
+		.catch(exc => {
+			let msg = 'Migration failed';
+			let data = {
+				httpStatusCode: exc.httpStatusCode,
+				message: exc + '',
+				stack: exc.stack,
+			};
+			console.error(msg, data);
+			return Diag.critical(msg, data);
+		});
 })();
