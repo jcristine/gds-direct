94435867ef09d0fbaef9ac4d4cdf975f01e5ea0d
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-03-02 13:35:53 +0000

    Refactor: move Sabre pricing command parser to a separate module
    It takes 186 lines, and this is not even complete implementation

--- backend/Transpiled/Gds/Parsers/Sabre/CommandParser.js
+++ backend/Transpiled/Gds/Parsers/Sabre/CommandParser.js
@@ -9,6 +9,7 @@ const TariffCmdParser = require('../../../Gds/Parsers/Sabre/Commands/TariffCmdPa
  * when we need, it also could return data to, for example, convert it to Apollo command
  */
 const php = require('../../../php.js');
+const PricingCmdParser = require("./Commands/PricingCmdParser");
 
 class CommandParser {
 	static detectCommandType($cmd) {
@@ -243,187 +244,6 @@ class CommandParser {
 		}
 	}
 
-	/** @param $expr = '1/3/5-7/9' */
-	static parseRanges($expr) {
-		let $parseRange;
-		$parseRange = ($text) => {
-			let $pair;
-			$pair = php.explode('-', $text);
-			return php.range($pair[0], $pair[1] || $pair[0]);
-		};
-		return Fp.flatten(Fp.map($parseRange, php.explode('\/', php.trim($expr))));
-	}
-
-	// 'N1.1/1.2/2.1'
-	// 'N1.1-2.1'
-	// 'N1.1-2.1/4.1-5.1/6.0'
-	// 'N1¥N2'
-	static parseNameQualifier($token) {
-		let $content, $records;
-		if (!StringUtil.startsWith($token, 'N')) {
-			return null;
-		} else {
-			$content = php.substr($token, 1);
-		}
-		$records = Fp.map(($ptcToken) => {
-			let $matches, $_, $from, $to;
-			if (php.preg_match(/^(\d+(?:\.\d+|))(-\d+(?:\.\d+|)|)$/, $ptcToken, $matches = [])) {
-				[$_, $from, $to] = $matches;
-				$to = php.substr($to, 1);
-				return {
-					'fieldNumber': php.explode('.', $from)[0],
-					'firstNameNumber': (php.explode('.', $from) || {})[1] || '0',
-					'through': $to ? {
-						'fieldNumber': php.explode('.', $to)[0],
-						'firstNameNumber': (php.explode('.', $to) || {})[1] || '0',
-					} : null,
-				};
-			} else {
-				return null;
-			}
-		}, php.explode('\/', $content));
-		if (Fp.any('is_null', $records)) {
-			return null;
-		} else {
-			return $records;
-		}
-	}
-
-	/**
-	 * @param $token = 'S1/2-3*Q//DA25*PC05' || 'S1/3'
-	 * @return [1,2,3]
-	 */
-	static parseSegmentQualifier($token) {
-		let $regex, $matches;
-		$regex =
-			'\/^S' +
-			'(?<segNums>\\d+[\\d\\\/\\-]*)' +
-			'(?<unparsed>\\*.+|)' +
-			'$\/';
-		if (php.preg_match($regex, $token, $matches = [])) {
-			return {
-				'segmentNumbers': this.parseRanges($matches['segNums']),
-				'unparsed': $matches['unparsed'],
-			};
-		} else {
-			return null;
-		}
-	}
-
-	// 'PADT', 'PINF'
-	// 'PADT/CMP' // companion
-	// 'PJCB/2JNF' // 1 JCB (adult) and 2 JNF (infants)
-	// 'P1ADT/2C11/1ADT'
-	static parsePtcQualifier($token) {
-		let $content, $records;
-		if (!StringUtil.startsWith($token, 'P')) {
-			return null;
-		} else {
-			$content = php.substr($token, 1);
-		}
-		$records = Fp.map(($ptcToken) => {
-			let $matches, $_, $cnt, $ptc;
-			if (php.preg_match(/^(\d*)([A-Z0-9]{3})$/, $ptcToken, $matches = [])) {
-				[$_, $cnt, $ptc] = $matches;
-				$cnt = $cnt !== '' ? php.intval($cnt) : null;
-				return {'quantity': $cnt, 'ptc': $ptc};
-			} else {
-				return null;
-			}
-		}, php.explode('\/', $content));
-		if (Fp.any('is_null', $records)) {
-			return null;
-		} else {
-			return $records;
-		}
-	}
-
-	/**
-	 * @see https://formatfinder.sabre.com/Content/Pricing/PricingOptionalQualifiers.aspx?ItemID=7481cca11a7449a19455dc598d5e3ac9
-	 */
-	static parsePricingQualifier($token) {
-		let $name, $data, $matches, $_, $percentMarker, $region, $amount;
-		[$name, $data] = [null, null];
-		if ($token === 'RQ') {
-			[$name, $data] = ['createPriceQuote', true];
-		} else if ($token === 'ETR') {
-			[$name, $data] = ['areElectronicTickets', true];
-		} else if ($token === 'FXD') {
-			[$name, $data] = ['forceProperEconomy', true];
-		} else if (php.preg_match(/^MPC-(?<mpc>[A-Z0-9]+)$/, $token, $matches = [])) {
-			[$name, $data] = ['maxPenaltyForChange', {
-				'value': $matches['mpc'],
-			}];
-		} else if (php.in_array($token, ['PL', 'PV'])) {
-			[$name, $data] = ['fareType', $token === 'PL' ? 'public' : 'private'];
-		} else if ($data = this.parseNameQualifier($token)) {
-			$name = 'names';
-		} else if ($data = this.parsePtcQualifier($token)) {
-			$name = 'ptc';
-		} else if ($data = this.parseSegmentQualifier($token)) {
-			$name = 'segments';
-		} else if (php.preg_match(/^PU\*(\d*\.?\d+)(\/[A-Z0-9]+|)$/, $token, $matches = [])) {
-			[$name, $data] = ['markup', $matches[1]];
-		} else if (php.preg_match(/^K(P|)([A-Z]*)(\d*\.?\d+)$/, $token, $matches = [])) {
-			[$_, $percentMarker, $region, $amount] = $matches;
-			[$name, $data] = ['commission', {
-				'units': $percentMarker ? 'percent' : 'amount',
-				'region': $region || null,
-				'value': $amount,
-			}];
-		} else if (php.preg_match(/^A([A-Z0-9]{2})$/, $token, $matches = [])) {
-			[$name, $data] = ['validatingCarrier', $matches[1]];
-		} else if (php.preg_match(/^C-([A-Z0-9]{2})$/, $token, $matches = [])) {
-			[$name, $data] = ['governingCarrier', $matches[1]];
-		} else if (php.preg_match(/^M([A-Z]{3})$/, $token, $matches = [])) {
-			[$name, $data] = ['currency', $matches[1]];
-		} else if (php.preg_match(/^AC\*([A-Z0-9]+)$/, $token, $matches = [])) {
-			[$name, $data] = ['accountCode', $matches[1]];
-		} else if (php.preg_match(/^ST(\d+[\d\/\-]*)$/, $token, $matches = [])) {
-			[$name, $data] = ['sideTrip', this.parseRanges($matches[1])];
-		} else if (php.preg_match(/^W(TKT)$/, $token, $matches = [])) {
-			[$name, $data] = ['exchange', $matches[1]];
-		} else if (php.preg_match(/^NC$/, $token, $matches = [])) {
-			[$name, $data] = ['lowestFare', true];
-		} else if (php.preg_match(/^NCS$/, $token, $matches = [])) {
-			[$name, $data] = ['lowestFareIgnoringAvailability', true];
-		} else if (php.preg_match(/^NCB$/, $token, $matches = [])) {
-			[$name, $data] = ['lowestFareAndRebook', true];
-		} else if (php.preg_match(/^Q([A-Z][A-Z0-9]*)$/, $token, $matches = [])) {
-			[$name, $data] = ['fareBasis', $matches[1]];
-		}
-		return [$name, $data];
-	}
-
-	// TODO: move to a separate class
-	static parsePricingQualifiers($qualifiers) {
-		let $parsedQualifiers, $qualifier, $name, $data;
-		$parsedQualifiers = [];
-		for ($qualifier of Object.values($qualifiers ? php.explode('\u00A5', $qualifiers) : [])) {
-			[$name, $data] = this.parsePricingQualifier($qualifier);
-			$parsedQualifiers.push({
-				'raw': $qualifier,
-				'type': $name,
-				'parsed': $data,
-			});
-		}
-		return $parsedQualifiers;
-	}
-
-	static parsePriceItinerary($cmd) {
-		let $command, $data;
-		$command = php.substr($cmd, 0, 2);
-		if ($command === 'WP') {
-			$data = this.parsePricingQualifiers(php.substr($cmd, 2));
-			return {
-				'baseCmd': $command,
-				'pricingModifiers': $data,
-			};
-		} else {
-			return null;
-		}
-	}
-
 	/** @param $expr = '3' || '9-12' || '9,12' || '1-3,6-8,12'; */
 	static parseRemarkRanges($expr) {
 		let $parseRange;
@@ -647,7 +467,7 @@ class CommandParser {
 			$data = $matches[1];
 		} else if ($type = this.detectCommandType($cmd)) {
 			$data = null;
-		} else if ($data = this.parsePriceItinerary($cmd)) {
+		} else if ($data = PricingCmdParser.parse($cmd)) {
 			// Sabre's pricing command starts with WP, but since there are a lot of
 			// other Sabre commands that start with WP, like WPRD* or WPNI, we should
 			// match command as "priceItinerary" only if none of the other matched
---
+++ backend/Transpiled/Gds/Parsers/Sabre/Commands/PricingCmdParser.js
@@ -0,0 +1,186 @@
+
+const php = require('../../../../php.js');
+const StringUtil = require('../../../../Lib/Utils/StringUtil.js');
+const Fp = require('../../../../Lib/Utils/Fp.js');
+
+/** @param $expr = '1/3/5-7/9' */
+let parseRanges = ($expr) => {
+	let $parseRange;
+	$parseRange = ($text) => {
+		let $pair;
+		$pair = php.explode('-', $text);
+		return php.range($pair[0], $pair[1] || $pair[0]);
+	};
+	return Fp.flatten(Fp.map($parseRange, php.explode('\/', php.trim($expr))));
+};
+
+// 'N1.1/1.2/2.1'
+// 'N1.1-2.1'
+// 'N1.1-2.1/4.1-5.1/6.0'
+// 'N1¥N2'
+let parseNameQualifier = ($token) => {
+	let $content, $records;
+	if (!StringUtil.startsWith($token, 'N')) {
+		return null;
+	} else {
+		$content = php.substr($token, 1);
+	}
+	$records = Fp.map(($ptcToken) => {
+		let $matches, $_, $from, $to;
+		if (php.preg_match(/^(\d+(?:\.\d+|))(-\d+(?:\.\d+|)|)$/, $ptcToken, $matches = [])) {
+			[$_, $from, $to] = $matches;
+			$to = php.substr($to, 1);
+			return {
+				'fieldNumber': php.explode('.', $from)[0],
+				'firstNameNumber': (php.explode('.', $from) || {})[1] || '0',
+				'through': $to ? {
+					'fieldNumber': php.explode('.', $to)[0],
+					'firstNameNumber': (php.explode('.', $to) || {})[1] || '0',
+				} : null,
+			};
+		} else {
+			return null;
+		}
+	}, php.explode('\/', $content));
+	if (Fp.any('is_null', $records)) {
+		return null;
+	} else {
+		return $records;
+	}
+};
+
+/**
+ * @param $token = 'S1/2-3*Q//DA25*PC05' || 'S1/3'
+ * @return [1,2,3]
+ */
+let parseSegmentQualifier = ($token) => {
+	let $regex, $matches;
+	$regex =
+		'\/^S' +
+		'(?<segNums>\\d+[\\d\\\/\\-]*)' +
+		'(?<unparsed>\\*.+|)' +
+		'$\/';
+	if (php.preg_match($regex, $token, $matches = [])) {
+		return {
+			'segmentNumbers': parseRanges($matches['segNums']),
+			'unparsed': $matches['unparsed'],
+		};
+	} else {
+		return null;
+	}
+}
+
+// 'PADT', 'PINF'
+// 'PADT/CMP' // companion
+// 'PJCB/2JNF' // 1 JCB (adult) and 2 JNF (infants)
+// 'P1ADT/2C11/1ADT'
+let parsePtcQualifier = ($token) => {
+	let $content, $records;
+	if (!StringUtil.startsWith($token, 'P')) {
+		return null;
+	} else {
+		$content = php.substr($token, 1);
+	}
+	$records = Fp.map(($ptcToken) => {
+		let $matches, $_, $cnt, $ptc;
+		if (php.preg_match(/^(\d*)([A-Z0-9]{3})$/, $ptcToken, $matches = [])) {
+			[$_, $cnt, $ptc] = $matches;
+			$cnt = $cnt !== '' ? php.intval($cnt) : null;
+			return {'quantity': $cnt, 'ptc': $ptc};
+		} else {
+			return null;
+		}
+	}, php.explode('\/', $content));
+	if (Fp.any('is_null', $records)) {
+		return null;
+	} else {
+		return $records;
+	}
+};
+
+/**
+ * @see https://formatfinder.sabre.com/Content/Pricing/PricingOptionalQualifiers.aspx?ItemID=7481cca11a7449a19455dc598d5e3ac9
+ */
+let parsePricingQualifier = ($token) => {
+	let $name, $data, $matches, $_, $percentMarker, $region, $amount;
+	[$name, $data] = [null, null];
+	if ($token === 'RQ') {
+		[$name, $data] = ['createPriceQuote', true];
+	} else if ($token === 'ETR') {
+		[$name, $data] = ['areElectronicTickets', true];
+	} else if ($token === 'FXD') {
+		[$name, $data] = ['forceProperEconomy', true];
+	} else if (php.preg_match(/^MPC-(?<mpc>[A-Z0-9]+)$/, $token, $matches = [])) {
+		[$name, $data] = ['maxPenaltyForChange', {
+			'value': $matches['mpc'],
+		}];
+	} else if (php.in_array($token, ['PL', 'PV'])) {
+		[$name, $data] = ['fareType', $token === 'PL' ? 'public' : 'private'];
+	} else if ($data = parseNameQualifier($token)) {
+		$name = 'names';
+	} else if ($data = parsePtcQualifier($token)) {
+		$name = 'ptc';
+	} else if ($data = parseSegmentQualifier($token)) {
+		$name = 'segments';
+	} else if (php.preg_match(/^PU\*(\d*\.?\d+)(\/[A-Z0-9]+|)$/, $token, $matches = [])) {
+		[$name, $data] = ['markup', $matches[1]];
+	} else if (php.preg_match(/^K(P|)([A-Z]*)(\d*\.?\d+)$/, $token, $matches = [])) {
+		[$_, $percentMarker, $region, $amount] = $matches;
+		[$name, $data] = ['commission', {
+			'units': $percentMarker ? 'percent' : 'amount',
+			'region': $region || null,
+			'value': $amount,
+		}];
+	} else if (php.preg_match(/^A([A-Z0-9]{2})$/, $token, $matches = [])) {
+		[$name, $data] = ['validatingCarrier', $matches[1]];
+	} else if (php.preg_match(/^C-([A-Z0-9]{2})$/, $token, $matches = [])) {
+		[$name, $data] = ['governingCarrier', $matches[1]];
+	} else if (php.preg_match(/^M([A-Z]{3})$/, $token, $matches = [])) {
+		[$name, $data] = ['currency', $matches[1]];
+	} else if (php.preg_match(/^AC\*([A-Z0-9]+)$/, $token, $matches = [])) {
+		[$name, $data] = ['accountCode', $matches[1]];
+	} else if (php.preg_match(/^ST(\d+[\d\/\-]*)$/, $token, $matches = [])) {
+		[$name, $data] = ['sideTrip', parseRanges($matches[1])];
+	} else if (php.preg_match(/^W(TKT)$/, $token, $matches = [])) {
+		[$name, $data] = ['exchange', $matches[1]];
+	} else if (php.preg_match(/^NC$/, $token, $matches = [])) {
+		[$name, $data] = ['lowestFare', true];
+	} else if (php.preg_match(/^NCS$/, $token, $matches = [])) {
+		[$name, $data] = ['lowestFareIgnoringAvailability', true];
+	} else if (php.preg_match(/^NCB$/, $token, $matches = [])) {
+		[$name, $data] = ['lowestFareAndRebook', true];
+	} else if (php.preg_match(/^Q([A-Z][A-Z0-9]*)$/, $token, $matches = [])) {
+		[$name, $data] = ['fareBasis', $matches[1]];
+	}
+	return [$name, $data];
+};
+
+let parsePricingQualifiers = ($qualifiers) => {
+	let $parsedQualifiers, $qualifier, $name, $data;
+	$parsedQualifiers = [];
+	for ($qualifier of Object.values($qualifiers ? php.explode('\u00A5', $qualifiers) : [])) {
+		[$name, $data] = parsePricingQualifier($qualifier);
+		$parsedQualifiers.push({
+			'raw': $qualifier,
+			'type': $name,
+			'parsed': $data,
+		});
+	}
+	return $parsedQualifiers;
+};
+
+exports.parseModifier = (mod) => parsePricingQualifier(mod);
+
+exports.parse = ($cmd) => {
+	let $command, $data;
+	$command = php.substr($cmd, 0, 2);
+	if ($command === 'WP') {
+		$data = parsePricingQualifiers(php.substr($cmd, 2));
+		return {
+			'baseCmd': $command,
+			'pricingModifiers': $data,
+		};
+	} else {
+		return null;
+	}
+};
