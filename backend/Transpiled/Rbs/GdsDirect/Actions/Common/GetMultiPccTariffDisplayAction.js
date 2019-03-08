a840548358635a6b2e200b102639667e822cb0e2
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-03-08 18:08:44 +0000

    Use RBS API to get $D/MIX rules and to report results for FLST optimization

--- backend/Transpiled/Rbs/GdsDirect/Actions/Common/GetMultiPccTariffDisplayAction.js
+++ backend/Transpiled/Rbs/GdsDirect/Actions/Common/GetMultiPccTariffDisplayAction.js
@@ -17,9 +17,7 @@ const {allWrap, timeout} = require("../../../../../Utils/Misc");
 class GetMultiPccTariffDisplayAction {
 	constructor() {
 		this.$repriceRules = null;
-		this.$log = ($msg, $data) => {
-
-		};
+		this.$log = ($msg, $data) => {};
 		this.$geoProvider = new LocationGeographyProvider();
 		this.$baseDate = php.date('Y-m-d H:i:s');
 	}
@@ -53,36 +51,11 @@ class GetMultiPccTariffDisplayAction {
 
 	async getRepriceRules() {
 		if (!this.$repriceRules) {
-			// TODO: fetch from RBS
-			let apiData = {"records":[{"departure_items":[{"type":"country","value":"US","name":null}],"destination_items":[{"type":"region","value":"35","name":"Middle East"}],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"13NM","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G8P","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2I3L","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"U2E5","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""}],"id":"26"},{"departure_items":[{"type":"country","value":"US","name":null}],"destination_items":[{"type":"region","value":"39","name":"Latin America"}],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G2H","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G8P","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"13NM","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F9B","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"15D9","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"U2E5","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"amadeus","pcc":"LAXGO3106","ptc":"","account_code":"","fare_type":"private"},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""}],"id":"28"},{"departure_items":[{"type":"country","value":"US","name":null}],"destination_items":[{"type":"region","value":"34","name":"Africa"}],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"13NM","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G2H","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F9B","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"U2E5","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""}],"id":"30"},{"departure_items":[{"type":"country","value":"US","name":null}],"destination_items":[{"type":"region","value":"33","name":"Europe"}],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G2H","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"13NM","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"U2E5","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"amadeus","pcc":"LAXGO3106","ptc":"","account_code":"","fare_type":"private"},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""}],"id":"32"},{"departure_items":[{"type":"country","value":"US","name":null}],"destination_items":[{"type":"region","value":"37","name":"Oceania"}],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G2H","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"13NM","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"amadeus","pcc":"LAXGO3106","ptc":"","account_code":"","fare_type":"private"},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""}],"id":"34"},{"departure_items":[{"type":"country","value":"US","name":null}],"destination_items":[{"type":"region","value":"42","name":"India \u0026 ISC"}],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"13NM","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G2H","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2I3L","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"U2E5","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""}],"id":"36"},{"departure_items":[{"type":"country","value":"US","name":null}],"destination_items":[{"type":"region","value":"36","name":"Asia"}],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G2H","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2E8R","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"U2E5","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F9B","ptc":"JWZ","account_code":"","fare_type":""}],"id":"38"},{"departure_items":[{"type":"country","value":"CA","name":"Canada"}],"destination_items":[],"reprice_pcc_records":[{"gds":"apollo","pcc":"2BQ6","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2E4T","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2I70","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2E1I","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2ER7","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"10OW","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"RR8F","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5EGB","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"T42I","ptc":"JCB","account_code":"","fare_type":""},{"gds":"amadeus","pcc":"YTOGO310E","ptc":"","account_code":"","fare_type":"private"},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F9B","ptc":"JWZ","account_code":"","fare_type":""}],"id":"40"},{"departure_items":[{"type":"country","value":"GB","name":"United Kingdom"}],"destination_items":[],"reprice_pcc_records":[{"gds":"apollo","pcc":"13NM","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"0EKH","ptc":"ITX","account_code":"BSAG","fare_type":""},{"gds":"sabre","pcc":"0EKH","ptc":"","account_code":"BSAG","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"K2MI","ptc":"","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"galileo","pcc":"K9P","ptc":"ITX","account_code":"TPACK","fare_type":"private"},{"gds":"galileo","pcc":"K9P","ptc":"","account_code":"TPACK","fare_type":"private"},{"gds":"galileo","pcc":"G8T","ptc":"ITX","account_code":"TPACK","fare_type":"private"},{"gds":"galileo","pcc":"G8T","ptc":"","account_code":"TPACK","fare_type":"private"},{"gds":"galileo","pcc":"3ZV4","ptc":"ITX","account_code":"BSAG","fare_type":"private"},{"gds":"galileo","pcc":"3ZV4","ptc":"","account_code":"BSAG","fare_type":"private"}],"id":"42"},{"departure_items":[{"type":"country","value":"US","name":null}],"destination_items":[],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G2H","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"U2E5","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"6IIF","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F3K","ptc":"JWZ","account_code":"","fare_type":""}],"id":"44"},{"departure_items":[],"destination_items":[],"reprice_pcc_records":[{"gds":"apollo","pcc":"1O3K","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G52","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2F9B","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"13NM","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"apollo","pcc":"2G2H","ptc":"JWZ","account_code":"","fare_type":""},{"gds":"sabre","pcc":"U2E5","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"5E9H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"DK8H","ptc":"JCB","account_code":"","fare_type":""},{"gds":"sabre","pcc":"0EKH","ptc":"ITX","account_code":"BSAG","fare_type":"private"},{"gds":"sabre","pcc":"0EKH","ptc":"","account_code":"BSAG","fare_type":"private"},{"gds":"sabre","pcc":"K2MI","ptc":"JCB","account_code":"","fare_type":""},{"gds":"galileo","pcc":"K9P","ptc":"ITX","account_code":"TPACK","fare_type":"private"},{"gds":"galileo","pcc":"K9P","ptc":"","account_code":"TPACK","fare_type":"private"},{"gds":"galileo","pcc":"G8T","ptc":"ITX","account_code":"TPACK","fare_type":"private"},{"gds":"galileo","pcc":"G8T","ptc":"","account_code":"TPACK","fare_type":"private"}],"id":"46"}]};
+			let serviceRs = await RbsClient.getMultiPccTariffRules();
+			let apiData = serviceRs.result.result;
 			this.$repriceRules = apiData.records;
 		}
 		return Promise.resolve(this.$repriceRules);
-	}
-
-	// should those be defined in GUI with departure => null and destination => null maybe?
-	static getFallbackPccs() {
-		let $pccs, $reprices, $pccRec, $pccReprices;
-		$pccs = [
-			{'gds': 'apollo', 'pcc': '1O3K'},
-			{'gds': 'apollo', 'pcc': '2G52'},
-			{'gds': 'apollo', 'pcc': '2F9B'},
-			{'gds': 'apollo', 'pcc': '13NM'},
-			{'gds': 'apollo', 'pcc': '2G2H'},
-			{'gds': 'sabre', 'pcc': 'U2E5'},
-			{'gds': 'sabre', 'pcc': '5E9H'},
-			{'gds': 'sabre', 'pcc': 'DK8H'},
-			{'gds': 'sabre', 'pcc': '0EKH'},
-			{'gds': 'sabre', 'pcc': 'K2MI'},
-			{'gds': 'galileo', 'pcc': 'K9P'},
-			{'gds': 'galileo', 'pcc': 'G8T'},
-		];
-		$reprices = [];
-		for ($pccRec of Object.values($pccs)) {
-			$pccReprices = this.applyDefaultRepriceRules($pccRec['gds'], $pccRec['pcc']);
-			$reprices = php.array_merge($reprices, $pccReprices);
-		}
-		return $reprices;
 	}
 
 	static matchesLocation($airport, $locationItems, $geo) {
@@ -128,13 +101,16 @@ class GetMultiPccTariffDisplayAction {
 	async getPccs($cmdData, $sessionData) {
 		let $routeRules, $fallbackPccs, $rules, $rule, $pccs, $isCurrent;
 		$routeRules = [];
-		$fallbackPccs = this.constructor.getFallbackPccs();
+		$fallbackPccs = [];
 		$rules = await this.getRepriceRules();
 		for ($rule of Object.values($rules)) {
-			if ($rule['departure_items'] || $rule['destination_items']) {
+			if (!php.empty($rule['departure_items']) ||
+				!php.empty($rule['destination_items'])
+			) {
 				$routeRules.push($rule);
 			} else {
-				$fallbackPccs = $rule['reprice_pcc_records'];
+				$fallbackPccs = $rule['reprice_pcc_records']
+					.map(r => this.constructor.transformPccRecordFromDb(r));
 			}
 		}
 		let pccsFromRules = php.array_merge(
@@ -150,39 +126,6 @@ class GetMultiPccTariffDisplayAction {
 			$pccs.push({'gds': $sessionData['gds'], 'pcc': $sessionData['pcc']});
 		}
 		return $pccs;
-	}
-
-	// should not be needed I guess once we add _current_ PCC and fallback PCC-s in the GUI
-	static applyDefaultRepriceRules($gds, $pcc) {
-		let $extensions;
-		$extensions = [];
-		if ($gds === 'apollo') {
-			$extensions.push({'ptc': 'JWZ'});
-		} else if ($gds === 'sabre') {
-			if (php.in_array($pcc, ['6IIF', '5E9H', 'RR8F', '5EGB', 'DK8H', 'T42I', 'U2E5'])) {
-				$extensions.push({'ptc': 'JCB'});
-			}
-			if ($pcc === '0EKH') {
-				$extensions.push({'accountCode': 'BSAG', 'ptc': 'ITX'}); // FQLONNYC20SEP¥RR*BSAG¥PITX
-				$extensions.push({'accountCode': 'BSAG', 'ptc': null});  // FQLONNYC20SEP¥RR*BSAG
-			}
-		} else if ($gds === 'amadeus') {
-			$extensions.push({'fareType': 'private'}); // /R,U
-		} else if ($gds === 'galileo') {
-			if (php.in_array($pcc, ['K9P', 'G8T'])) {
-				$extensions.push({'accountCode': 'TPACK', 'fareType': 'private', 'ptc': 'ITX'}); // FDLONNYC25SEP*ITX-PRI-TPACK:P
-				$extensions.push({'accountCode': 'TPACK', 'fareType': 'private', 'ptc': null});  // FD2OCTLONWAS-PRI-TPACK:P
-			}
-			if ($pcc === '3ZV4') {
-				$extensions.push({'accountCode': 'BSAG', 'fareType': 'private', 'ptc': 'ITX'}); // FDLONNYC25SEP*ITX-PRI-TPACK:P
-				$extensions.push({'accountCode': 'BSAG', 'fareType': 'private', 'ptc': null});  // FD2OCTLONWAS-PRI-TPACK:P
-			}
-		}
-		return Fp.map(($ext) => {
-			return php.array_merge({
-				'gds': $gds, 'pcc': $pcc,
-			}, $ext);
-		}, $extensions.length > 0 ? $extensions : [[]]);
 	}
 
 	static extendFromPccRecord($rpcParams, $pccRec, $sessionData) {
@@ -276,13 +219,19 @@ class GetMultiPccTariffDisplayAction {
 		this.log('Got ' + php.count($finishedJobs) + ' of ' + php.count($startedJobs) + ' jobs done: ' +
 			php.implode(', ', php.array_column($finishedJobs, 'pcc')), $finishedJobs);
 		$finishedIds = php.array_column($finishedJobs, 'jobId');
-		$unfinished = Fp.filter(($started) => {
-			return !php.in_array($started['jobId'], $finishedIds);
-		}, $startedJobs);
-		if ($unfinished) {
+		$unfinished = $startedJobs.filter(($started) =>
+			!php.in_array($started['jobId'], $finishedIds));
+		if (!php.empty($unfinished)) {
 			this.log('WARNING: Got ' + php.count($unfinished) + ' undone jobs: ' +
 				php.implode(', ', php.array_column($unfinished, 'pcc')), $unfinished);
 		}
+		RbsClient.reportMultiPccTariffResult({
+			jobsStarted: $startedJobs.length,
+			jobs: $finishedJobs.map(j => ({
+				params: j.rpcParams,
+				result: j.jobResult.result,
+			})).filter(j => j.result),
+		});
 	}
 
 	/** unlike array_diff_assoc, this function does not throw NOTICE-s on nested arrays */
@@ -327,6 +276,7 @@ class GetMultiPccTariffDisplayAction {
 				.then(serviceRs => ({
 					pcc: $rpcParams.pcc,
 					gds: $rpcParams.gds,
+					rpcParams: $rpcParams,
 					jobResult: serviceRs.result,
 				}));
 
