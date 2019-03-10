4b000e820734cd0aa5c50dc509d134aafbffffc9
Artur Klesun <arturklesun@gmail.com>
2019-03-10 21:08:59 +0200

    Use correct db cmd row col names in MoveDownAllAction.js

--- backend/Transpiled/Rbs/GdsDirect/Actions/Amadeus/MoveDownAllAction.js
+++ backend/Transpiled/Rbs/GdsDirect/Actions/Amadeus/MoveDownAllAction.js
@@ -16,15 +16,15 @@ class MoveDownAllAction extends AbstractGdsAction {
 			$pager, $fullCalledCommand;
 
 		$usedMdCmds = SessionStateProcessor.mrCmdTypes;
-		$mdCmdRows = $cmdLog.getLastCommandsOfTypes($usedMdCmds);
+		$mdCmdRows = await $cmdLog.getLastCommandsOfTypes($usedMdCmds);
 		if (!($originalCmdRow = php.array_shift($mdCmdRows))) {
 			// if session just started for example
 			return {'userMessages': ['There is nothing to scroll']};
 		}
-		if (!($format = AmadeusUtils.guessFormatFromCmd($originalCmdRow['cmd_performed']))) {
-			return {'errors': ['Unsupported scrolling format - ' + $originalCmdRow['cmd_performed']]};
+		if (!($format = AmadeusUtils.guessFormatFromCmd($originalCmdRow['cmd']))) {
+			return {'errors': ['Unsupported scrolling format - ' + $originalCmdRow['cmd']]};
 		}
-		$usedMdCmds = php.array_values(php.array_unique(php.array_column($mdCmdRows, 'cmd_performed')));
+		$usedMdCmds = php.array_values(php.array_unique(php.array_column($mdCmdRows, 'cmd')));
 		if (!php.empty($wrongMds = php.array_diff($usedMdCmds, [$format['moveRestCmd']]))) {
 			// probably could do >MT; and fetch from start in such case
 			return {'errors': ['Mixed MD commands are not supported - got ' + php.implode(', ', $wrongMds) + ', expected only ' + $format['moveRestCmd']]};
@@ -35,19 +35,25 @@ class MoveDownAllAction extends AbstractGdsAction {
 			php.array_column($mdCmdRows, 'output')
 		);
 
-		$i = 0;
+		$i = 1;
 		$cleanPages = [];
 		$hasMore = true;
-		while ($hasMore && $i++ < $limit) {
+		let done = false;
+		while (!done) {
 			$page = php.array_shift($pages)
 				|| (await this.runCmd($format['moveRestCmd'])).output;
 			$pager = $format['parsePager']($page);
-			$cleanPages.push($pager['content']);
 			$hasMore = $pager['hasMore'];
+			done = !$hasMore || $i++ >= $limit;
+			if (done) {
+				$cleanPages.push($page); // to keep pager
+			} else {
+				$cleanPages.push($pager['content']);
+			}
 		}
 
 		$fullCalledCommand = {
-			'cmd': $originalCmdRow['cmd_performed'],
+			'cmd': $originalCmdRow['cmd'],
 			'output': php.implode(php.PHP_EOL, $cleanPages),
 		};
 
