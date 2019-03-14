41084ee1427ca00aa6b3f4a1ad5205539e737b1b
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-03-14 18:06:01 +0000

    Allow everyone to paste itinerary

--- backend/DataFormats/Wrappers/Agent.js
+++ backend/DataFormats/Wrappers/Agent.js
@@ -47,7 +47,7 @@ let Agent = (emcUser) => {
 		canSavePnrWithoutLead: () => hasRole('NEW_GDS_DIRECT_NO_LEAD_PNR'),
 		canOpenPrivatePnr: () => hasRole('NEW_GDS_DIRECT_PRIVATE_PNR_ACCESS'),
 		canUseMultiPccTariffDisplay: () => hasRole('NEW_GDS_DIRECT_MULTI_PCC_TARIFF_DISPLAY'),
-		canPasteItinerary: () => hasRole('NEW_GDS_DIRECT_PASTE_ITINERARY'),
+		canPasteItinerary: () => true, // hasRole('NEW_GDS_DIRECT_PASTE_ITINERARY'),
 		canUseMco: () => hasRole('NEW_GDS_DIRECT_HHMCO'),
 	};
 };
