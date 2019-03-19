df7310c67a9a5dba2b52ee31768cd5923ecaae6f
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-03-19 15:34:55 +0000

    Add 5 seconds timeout before starting server.js
    Not just in the Migration.js, since dying workers cause problems in other places as well

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 
+let Diag = require('./backend/LibWrappers/Diag.js');
+let Migration = require('./backend/Maintenance/Migration.js');
+let KeepAlive = require("./backend/Maintenance/KeepAlive");
+let Config = require("./backend/Config.js");
+
 (async () => {
+	if (Config.production) {
+		// a workaround for an extra job that gets spawned randomly and dies
+		Diag.log('Waiting for 5 seconds before starting server, pid ' + process.pid);
+		await new Promise(resolve => setTimeout(resolve, 5000));
+	}
 	console.log('initializing Web Routes');
 	require('./backend/WebRoutes.js');
-	let Diag = require('./backend/LibWrappers/Diag.js');
-	let Migration = require('./backend/Maintenance/Migration.js');
-	let KeepAlive = require("./backend/Maintenance/KeepAlive");
 	Migration.run()
 		.then(result => {
 			console.log('Migration was successful\n', JSON.stringify(result));
