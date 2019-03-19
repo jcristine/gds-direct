1a62a35fa20f5150e401cd97dd0a0c55a0b5bad7
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-03-19 21:40:47 +0000

    Remove unused code

--- bg_data_worker.js
+++ bg_data_worker.js
@@ -3,17 +3,6 @@ const KeepAlive = require("./backend/Maintenance/KeepAlive");
 let FluentLogger = require('./backend/LibWrappers/FluentLogger.js');
 const Config = require("./backend/Config");
 
-let workerLogId = FluentLogger.logNewId('bgworker');
-
-let log = (msg, ...data) => {
-	FluentLogger.logit(msg, workerLogId, ...data);
-	if (!Config.production) {
-		console.log((new Date()).toISOString() + ': ' + msg, ...data);
-	}
-};
-
-log('BG worker started ' + workerLogId);
-
 // probably should restart it every
 // minute in case it fails or something...
 let keepAlive = KeepAlive.run();
