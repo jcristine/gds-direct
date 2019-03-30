13e292be846a35cd9014a418360978584bfc1176
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-03-30 23:29:54 +0000

    Traverse tests dir recursively instead of listing every single test by hand

--- tests/runTests.js
+++ tests/runTests.js
@@ -1,59 +1,30 @@
 
 const {getConfig} = require('../backend/Config');
+const { resolve } = require('path');
+const { readdir, stat } = require('fs').promises;
+
 
 console.log('Starting unit tests');
 let args = process.argv.slice(process.execArgv.length + 2);
 
-// should probably load them implicitly like phpunit does...
-let testPromises = [
-	Promise.resolve([() => 3 > 4 ? 'Your code says that 3 > 4, this is wrong' : null]),
-	Promise.resolve([() => 5 < 4 ? Promise.resolve('Five can not be less than four, silly') : null]),
-	new (require('./backend/Transpiled/Gds/Parsers/Galileo/Pricing/FqParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/HteParser/HteParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/HteParser/TicketListParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/HteParser/TicketParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Rbs/GdsDirect/Actions/Apollo/ProcessApolloTerminalInputActionTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Rbs/GdsDirect/Actions/Apollo/ImportPqApolloActionTest.js'))().getTests(),
-	new (require('./backend/Actions/ExchangeApolloTicketTest.js'))().getTests(),
-	new (require('./backend/GdsHelpers/TravelportUtilsTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/Pnr/ItineraryParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Sabre/Pnr/ItineraryParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/Pnr/CommonParsersHelperTest.js'))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Apollo/CommandParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Sabre/CommandParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Amadeus/CommandParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Galileo/CommandParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Apollo/Pnr/PnrParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Sabre/Pnr/PnrParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Galileo/Pnr/PnrParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Amadeus/Pnr/PnrParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Rbs/GdsDirect/SessionStateProcessor/SessionStateProcessorTest"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Apollo/PricingParserTest"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Sabre/Pricing/SabrePricingParserTest"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Apollo/AirAvailabilityParserTest"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Sabre/AirAvailabilityParserTest"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Amadeus/PnrSearchParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/Gds/Parsers/Amadeus/WorkAreaScreenParserTest.js"))().getTests(),
-	new (require("./backend/Transpiled/App/Services/TerminalHighlightServiceTest"))().getTests(),
-	new (require("./backend/Transpiled/App/Services/TerminalServiceTest"))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/PnrHistoryParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/TariffDisplay/TariffDisplayParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/ApolloSvcParser/ApolloSvcParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Gds/Parsers/Apollo/ApolloVitParser/ApolloVitParserTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Rbs/GdsDirect/Actions/Apollo/DisplayHistoryActionHelperTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Rbs/GdsDirect/Actions/Common/MakeMultiPccTariffDumpActionTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Rbs/GdsDirect/Actions/Common/GetMultiPccTariffDisplayActionTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Rbs/GdsAction/ApolloMakeMcoActionTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Rbs/Process/Apollo/ImportPnr/Actions/RetrieveFlightServiceInfoActionTest.js'))().getTests(),
-	new (require('./backend/Transpiled/Rbs/GdsDirect/DialectTranslator/GdsDialectTranslatorTest.js'))().getTests(),
-];
+let getFiles = async function* (dir) {
+	const subdirs = await readdir(dir);
+	for (const subdir of subdirs) {
+		const res = resolve(dir, subdir);
+		if ((await stat(res)).isDirectory()) {
+			yield* getFiles(res);
+		} else {
+			yield res;
+		}
+	}
+};
 
 let oks = 0;
 let errors = [];
 
-let perform = async (testPromises) => {
-	let config = await getConfig();
+let perform = async () => {
 
+	let config = await getConfig();
 	config.external_service = {};
 	config.RBS_PASSWORD = null;
 	// TODO: get rid of taking highlight rules from DB and uncomment following
@@ -63,20 +34,37 @@ let perform = async (testPromises) => {
 	//config.HTTP_PORT = null;
 	//config.HOST = null;
 
-	for (let whenTests of testPromises) {
-		let tests = await whenTests;
-		for (let test of tests) {
-			let error = await test();
-			if (error) {
-				errors.push(error);
-				if (args.includes('debug')) {
-					console.error(error);
-					process.exit(-100);
-				}
-			} else {
-				++oks;
-			}
+	let i = 0;
+	for await (let file of getFiles(__dirname + '/backend/')) {
+		if (file === __dirname + '/backend/Transpiled/Lib/TestCase.js' ||
+			file === __dirname + '/backend/Transpiled/php.js'
+		) {
+			// placed there as a workaround for transpilation, as root
+			// namespace of tests is different than that of normal files
+			continue;
 		}
+		//console.log('Test Suit #' + i + ' ' + file);
+		try {
+			let testCls = require(file);
+			/** @type TestCase */
+			let testInst = new testCls();
+			let tests = await testInst.getTests();
+			for (let test of tests) {
+				let error = await test();
+				if (error) {
+					errors.push(error);
+					if (args.includes('debug')) {
+						console.error(error);
+						process.exit(-100);
+					}
+				} else {
+					++oks;
+				}
+			}
+		} catch (exc) {
+			errors.push('Test Suit #' + i + ' ' + file + ' failed to load - ' + exc);
+		}
+		++i;
 	}
 
 	console.log('\nFinished with ' + oks + ' oks and ' + errors.length + ' errors');
@@ -92,4 +80,4 @@ let perform = async (testPromises) => {
 	}
 };
 
-perform(testPromises);
+perform();
