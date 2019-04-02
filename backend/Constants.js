38cd608b824ba52c3d98411fda14cdfea4ae7a2f
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-04-02 18:52:59 +0000

    Add CMS main developers to the list of dev access users
    And add description for each person in the list

--- backend/Constants.js
+++ backend/Constants.js
@@ -3,13 +3,16 @@
 exports.supportedGdses = ['apollo', 'sabre', 'galileo', 'amadeus'];
 
 exports.admins = [
-	6206, // aklesuns
-	785, // kira
-	836, // Bruce
-	1330, // Bruce Paulson
-	20744, // Eldar
-	1092, // Jayden
-	3021, // Louis, Jayden's assistant
-	101395, // aprokopcuks
-	5820, // Rico
+	6206  , // aklesuns     , main developer
+	785   , // kira         , sales supervisor, participates in testing
+	2911  , // Paris        , sales supervisor, participates in testing
+	1330  , // Bruce Paulson, travel main business guy
+	20744 , // Eldar        , the business guy that contributed a lot to original GDS Direct development
+	1092  , // Jayden       , supervisor of experts (they are main users of the app)
+	3021  , // Louis        , an expert, Jayden's assistant, participates in testing
+	101395, // aprokopcuks  , RBS lead - deals with GDS-es like we do
+	5820  , // Rico         , ticketing guru that helps us developing masks, etc...
+	20021 , // inosov       , CMS core developer, needs our logs for debug
+	20059 , // Vdovichenko  , CMS lead, needs our logs for debug
+	2038  , // reppo        , travel CTO, needs our logs for debug
 ];
