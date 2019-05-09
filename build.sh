38e93f4edb362c196d06a3e68168ef17dc6c06ae
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-05-09 13:59:04 +0000

    Ok, return back to npm since yarn is trash too
    https://github.com/yarnpkg/yarn/issues/3630

--- build.sh
+++ build.sh
@@ -4,11 +4,10 @@
 # lessc public/_style/less/main.less > public/_style/css/parsed.css
 
 # downloads /node_modules/ if needed
-yarn install --frozen-lockfile --no-progress
+npm install --from-lock-file --no-progress
 
 startpath=$(pwd)
 cd public
-# gosh, yarn surely is fast, would be nice to use it here too instead of npm
 npm install --from-lock-file --no-progress
 node node_modules/webpack/bin/webpack.js --sort-modules-by size
 cd $startpath
