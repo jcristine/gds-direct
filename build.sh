2aac8fa8e3ca2d3fe4cb5ab9aa9d38219a319daa
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-05-09 10:55:38 +0000

    Use yarn instead of npm for backend

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@
 # lessc public/_style/less/main.less > public/_style/css/parsed.css
 
 # downloads /node_modules/ if needed
-npm install --from-lock-file --no-progress
+yarn install --frozen-lockfile --no-progress
 
 startpath=$(pwd)
 cd public
+# gosh, yarn surely is fast, would be nice to use it here too instead of npm
 npm install --from-lock-file --no-progress
 node node_modules/webpack/bin/webpack.js --sort-modules-by size
 cd $startpath
