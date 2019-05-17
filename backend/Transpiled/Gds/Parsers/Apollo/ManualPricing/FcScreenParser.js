c8304b477149b7bfa5d675ca4f0b7ab9431c19d0
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-05-17 09:41:34 +0000

    Transpile FcScreenParser.js

---
+++ backend/Transpiled/Gds/Parsers/Apollo/ManualPricing/FcScreenParser.js
@@ -0,0 +1,127 @@
+// namespace Gds\Parsers\Apollo\ManualPricing;
+
+const Fp = require('../../../../Lib/Utils/Fp.js');
+const StringUtil = require('../../../../Lib/Utils/StringUtil.js');
+const FareConstructionParser = require('../../../../Gds/Parsers/Common/FareConstruction/FareConstructionParser.js');
+
+/**
+ * parses output of >$FC{ptcNumber}/{storeNumber}
+ * it is a fare construction of a manual pricing
+ */
+const php = require('../../../../php.js');
+
+class FcScreenParser {
+	//'STL KL X/MSP KL X/AMS KL ATH M151.00KL. ',
+	//'X/AMS KL X/ATL KL STL M151.00NUC302.00............. ',
+	//'................................................... ',
+	//'................................................... ',
+	//'...........................END XFSTL4.5MSP4.5ATL4.5',
+	static parseMaskFareConstruction($text) {
+		let $regex, $matches, $raw, $fcRecord, $result, $error;
+
+		$regex =
+			'/^\\s*' +
+			'(?<mainPart>(?:.|\\s)+?)' +
+			'(?<space>[\\.\\s]+)' +
+			'(?<endingPart>END.*?)' +
+			'\\s*$/';
+
+		if (php.preg_match($regex, $text, $matches = [])) {
+			$raw = $matches['mainPart'] + ' ' + $matches['endingPart'];
+			$raw = php.preg_replace(/\.+\s/, '  ', $raw); // line may end like 'KL. '
+			$raw = php.preg_replace(/\s\n/, '', $raw); // join into single line
+			$fcRecord = FareConstructionParser.parse($raw);
+			$result = {'raw': $raw};
+			if ($error = $fcRecord['error']) {
+				$result['error'] = $error;
+			} else {
+				$result['parsed'] = $fcRecord['parsed'];
+			}
+			return $result;
+		} else {
+			return {'error': 'Failed to extract regex from mask'};
+		}
+	}
+
+	// ' CA54XXXXXXXXXXXX44 EXP0517/ M 005230 '
+	static parseCreditCard($text) {
+		let $regex, $matches, $indexes;
+
+		$regex =
+			'/\\s*' +
+			'(?<creditCardCompany>[A-Z]{2})' +
+			'(?<creditCardNumber>[^\\s]{15,16})\\s+EXP' +
+			'(?<expirationMonth>\\d{2})' +
+			'(?<expirationYear>\\d{2})' +
+			'(?<unparsed>(?:.|\\s)*?)' +
+			'\\s*$/';
+
+		if (php.preg_match($regex, $text, $matches = [])) {
+			$indexes = Fp.filter('is_numeric', php.array_keys($matches));
+			return php.array_diff_key($matches, php.array_flip($indexes));
+		} else {
+			return null;
+		}
+	}
+
+	// ' CHECK '
+	static parseFopText($text) {
+		let $parsed, $type;
+
+		$parsed = null;
+		if (php.trim($text) === 'CHECK') {
+			$type = 'check';
+		} else if ($parsed = this.parseCreditCard($text)) {
+			$type = 'creditCard';
+		} else {
+			$type = null;
+		}
+		return {
+			'raw': php.trim($text),
+			'type': $type,
+			'parsed': $parsed,
+		};
+	}
+
+	static parse($dump) {
+		let $lines, $header, $removeSemicolon, $textLeft, $regex, $matches, $cleanFcRecord, $maskFcRecord;
+
+		$lines = StringUtil.lines($dump);
+		$header = php.array_shift($lines);
+		if (!StringUtil.contains($header, 'FARE CONSTRUCTION')) {
+			return {'error': 'Unexpected start of dump - ' + php.trim($header)};
+		}
+
+		$removeSemicolon = ($line) => php.substr($line, 1);
+		$lines = Fp.map($removeSemicolon, $lines);
+		$textLeft = php.implode(php.PHP_EOL, $lines);
+
+		$regex =
+			'/^\\s*' +
+			'FP(?<fopText>(?:.|\\s)*?)\\s+' +
+			'FC;(?<maskFcText>(?:.|\\s)*?);\\s*' +
+			'(?<cleanFcText>(?:.|\\s)*?)' +
+			'\\s*$/';
+
+		if (php.preg_match($regex, $textLeft, $matches = [])) {
+			$cleanFcRecord = FareConstructionParser.parse($matches['cleanFcText']);
+			$maskFcRecord = this.parseMaskFareConstruction($matches['maskFcText']);
+			return {
+				'formOfPayment': this.parseFopText($matches['fopText']),
+				'hasFareConstruction': $cleanFcRecord['parsed'] || $maskFcRecord['parsed'] || null ? true : false,
+				'fareConstruction': {
+					'mask': $maskFcRecord,
+					'clean': {
+						'raw': php.trim($matches['cleanFcText']),
+						'parsed': $cleanFcRecord['parsed'],
+						'error': $cleanFcRecord['error'],
+					},
+				},
+			};
+		} else {
+			return {'error': 'Invalid dump structure:' + php.PHP_EOL + $textLeft};
+		}
+	}
+}
+
+module.exports = FcScreenParser;
---
+++ tests/backend/Transpiled/Gds/Parsers/Apollo/ManualPricing/FcScreenParserTest.js
@@ -0,0 +1,240 @@
+// namespace Gds\Parsers\Apollo\ManualPricing;
+
+const php = require('../../../../php.js');
+const FcScreenParser = require("../../../../../../../backend/Transpiled/Gds/Parsers/Apollo/ManualPricing/FcScreenParser");
+
+class FcScreenParserTest extends require('../../../../Lib/TestCase.js') {
+	provideDumps() {
+		let $list;
+
+		$list = [];
+
+		// >*ZQXV5Q; with "." treated as space in "00KL. "
+		$list.push([
+			php.implode(php.PHP_EOL, [
+				'>$FC/ATB FARE CONSTRUCTION ',
+				' FP CHECK FC;STL KL X/MSP KL X/AMS KL ATH M151.00KL. ',
+				';X/AMS KL X/ATL KL STL M151.00NUC302.00............. ',
+				';................................................... ',
+				';................................................... ',
+				';...........................END XFSTL4.5MSP4.5ATL4.5;',
+				';STL KL X/MSP KL X/AMS KL ATH M151.00KL X/AMS KL X/ATL KL ',
+				';STL M151.00NUC302.00 XFSTL4.5MSP4.5ATL4.5    ',
+			]),
+			{
+				'formOfPayment': {'type': 'check'},
+				'hasFareConstruction': true,
+				'fareConstruction': {
+					'mask': {
+						'raw': 'STL KL X/MSP KL X/AMS KL ATH M151.00KL X/AMS KL X/ATL KL STL M151.00NUC302.00 END XFSTL4.5MSP4.5ATL4.5',
+						'parsed': {
+							'segments': [
+								{'destination': 'MSP'},
+								{'destination': 'AMS'},
+								{'destination': 'ATH', 'fare': '151.00'},
+								{'destination': 'AMS'},
+								{'destination': 'ATL'},
+								{'destination': 'STL', 'fare': '151.00'},
+							],
+							'currency': 'NUC',
+							'fare': '302.00',
+							'facilityCharges': [
+								{'airport': 'STL', 'amount': '4.5'},
+								{'airport': 'MSP', 'amount': '4.5'},
+								{'airport': 'ATL', 'amount': '4.5'},
+							],
+						},
+					},
+					'clean': {
+						'parsed': {
+							'segments': [
+								{'destination': 'MSP'},
+								{'destination': 'AMS'},
+								{'destination': 'ATH', 'fare': '151.00'},
+								{'destination': 'AMS'},
+								{'destination': 'ATL'},
+								{'destination': 'STL', 'fare': '151.00'},
+							],
+							'currency': 'NUC',
+							'fare': '302.00',
+							'facilityCharges': [
+								{'airport': 'STL', 'amount': '4.5'},
+								{'airport': 'MSP', 'amount': '4.5'},
+								{'airport': 'ATL', 'amount': '4.5'},
+							],
+						},
+					},
+				},
+			},
+		]);
+
+		// >*SW8890; - without fare construction
+		$list.push([
+			php.implode(php.PHP_EOL, [
+				'>$FC/ATB FARE CONSTRUCTION ',
+				' FP CA54XXXXXXXXXXXX44 EXP0517/ M 005230 FC;........ ',
+				';................................................... ',
+				';................................................... ',
+				';................................................... ',
+				';.......................................END XFIAD4.5;',
+				';XFIAD4.5    ',
+			]),
+			{
+				'formOfPayment': {
+					'type': 'creditCard',
+					'parsed': {
+						'creditCardCompany': 'CA',
+						'creditCardNumber': '54XXXXXXXXXXXX44',
+						'expirationMonth': '05',
+						'expirationYear': '17',
+						'unparsed': '/ M 005230',
+					},
+				},
+				'hasFareConstruction': false,
+				'fareConstruction': {
+					'mask': {
+						'raw': '  END XFIAD4.5',
+					},
+					'clean': {
+						'raw': 'XFIAD4.5',
+					},
+				},
+			},
+		]);
+
+		// >*P2R032; shows that masked fare construction should be joined without spaces - "PAR 15M86.25"
+		$list.push([
+			php.implode(php.PHP_EOL, [
+				'>$FC/ATB FARE CONSTRUCTION ',
+				' FP CHECK FC;CLE DL X/MSP KL X/E/AMS KL FLR AF PAR 1 ',
+				';5M86.25AF X/NYC AF CLE M75.00 1S100.00 NUC261.25... ',
+				';................................................... ',
+				';................................................... ',
+				';...........................END XFCLE4.5MSP4.5JFK4.5;',
+				';CLE DL X/MSP KL X/E/AMS KL FLR AF PAR 15M86.25AF X/NYC AF ',
+				';CLE M75.00 1S100.00 NUC261.25 XFCLE4.5MSP4.5JFK4.5    ',
+			]),
+			{
+				'fareConstruction': {
+					'mask': {
+						'raw': php.implode(php.PHP_EOL, [
+							'CLE DL X/MSP KL X/E/AMS KL FLR AF PAR 15M86.25AF X/NYC AF CLE M75.00 1S100.00 NUC261.25 END XFCLE4.5MSP4.5JFK4.5',
+						]),
+						'parsed': {
+							'segments': [
+								{'destination': 'MSP'},
+								{'destination': 'AMS'},
+								{'destination': 'FLR'},
+								{'destination': 'PAR', 'mileageSurcharge': '15M', 'fare': '86.25'},
+								{'destination': 'NYC'},
+								{
+									'destination': 'CLE',
+									'fare': '75.00',
+									'stopoverFees': [{'amount': '100.00'}],
+								},
+							],
+							'currency': 'NUC',
+							'fare': '261.25',
+						},
+					},
+				},
+			},
+		]);
+
+		// >*Z92N6Q; - 'PC131.00.NUC656.00', likely our parser does better job
+		// extracting FC from mask than apollo themselves. Not sure this must be parsed.
+		$list.push([
+			php.implode(php.PHP_EOL, [
+				'>$FC/ATB FARE CONSTRUCTION ',
+				' FP CAXXXXXXXXXXXX1075 EXP0820/ M 01622Z FC;CHI CX X ',
+				';/HKG CX MNL 262.50 CX X/HKG CX CHI 262.50 PC131.00. ',
+				';NUC656.00.......................................... ',
+				';................................................... ',
+				';.......................................END XFORD4.5;',
+				';CHI CX X/HKG CX MNL 262.50 CX X/HKG CX CHI 262.50 ',
+				';PC131.00.NUC656.00 XFORD4.5    ',
+			]),
+			{
+				'formOfPayment': {
+					'type': 'creditCard',
+					'parsed': {
+						'creditCardCompany': 'CA',
+						'creditCardNumber': 'XXXXXXXXXXXX1075',
+						'expirationMonth': '08',
+						'expirationYear': '20',
+					},
+				},
+				'hasFareConstruction': true,
+				'fareConstruction': {
+					'mask': {
+						'raw': 'CHI CX X/HKG CX MNL 262.50 CX X/HKG CX CHI 262.50 PC131.00 NUC656.00 END XFORD4.5',
+						'parsed': {
+							'segments': [
+								{'airline': 'CX', 'destination': 'HKG'},
+								{'airline': 'CX', 'destination': 'MNL', 'fare': '262.50'},
+								{'airline': 'CX', 'destination': 'HKG'},
+								{'airline': 'CX', 'destination': 'CHI', 'fare': '262.50'},
+							],
+							'markup': '131.00',
+							'currency': 'NUC',
+							'fareAndMarkupInNuc': '656.00',
+							'facilityCharges': [{'airport': 'ORD', 'amount': '4.5'}],
+						},
+					},
+					'clean': {
+						'raw': php.implode(php.PHP_EOL, [
+							'CHI CX X/HKG CX MNL 262.50 CX X/HKG CX CHI 262.50 ',
+							'PC131.00.NUC656.00 XFORD4.5',
+						]),
+						'error': 'failed to completely parse fare construction in 1 attempts on .NUC656',
+					},
+				},
+			},
+		]);
+
+		// not filled mask example
+		$list.push([
+			php.implode(php.PHP_EOL, [
+				">$FC/ATB FARE CONSTRUCTION ",
+				" FP NO FOP FC;...................................... ",
+				";................................................... ",
+				";................................................... ",
+				";..................................................  ",
+				";...................................................;",
+				"",
+				";10DEC JFK PR MNL 100.00 $100.00     ",
+			]),
+			{
+				formOfPayment: {raw: 'NO FOP'},
+				hasFareConstruction: false,
+				fareConstruction: {
+					clean: {
+						raw: '10DEC JFK PR MNL 100.00 $100.00',
+					},
+				},
+			},
+		]);
+
+		return $list;
+	}
+
+	/**
+	 * @test
+	 * @dataProvider provideDumps
+	 */
+	testParser($dump, $expected) {
+		let $actual;
+
+		$actual = FcScreenParser.parse($dump);
+		this.assertArrayElementsSubset($expected, $actual);
+	}
+
+	getTestMapping() {
+		return [
+			[this.provideDumps, this.testParser],
+		];
+	}
+}
+
+FcScreenParserTest.count = 0;
+module.exports = FcScreenParserTest;
