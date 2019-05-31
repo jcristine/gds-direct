6d04848d6bd62e56737617f1e945c26da237ada2
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-05-31 15:13:17 +0000

    Use real config lan on dev

--- dev/env.js
+++ dev/env.js
@@ -1,9 +1,10 @@
 
 process.env.HTTP_PORT = '3012';
 process.env.SOCKET_PORT = '3022';
-process.env.REDIS_CLUSTER_NAME = "some-grect-redis";
+process.env.DB_NAME = "gds_direct_plus";
+process.env.REDIS_CLUSTER_NAME = "stage-travel-shared1";
 process.env.RANDOM_KEY = "12345678901234567890123456789012";
-process.env.CONFIG_LAN = "http://intranet.dyninno.net/~aklesuns/fake_config_lan/grect/";
+process.env.CONFIG_LAN = "https://config-lan.sandbox.dyninno.net";
 process.env.NODE_ENV = 'development';
 process.env.HOST = '0.0.0.0';
 process.env.RBS_PASSWORD = "qwerty";
