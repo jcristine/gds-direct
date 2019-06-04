718561fcd09b2ef19f95e992812e7144239d5eaa
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-06-04 13:59:32 +0000

    Fix deploy.sh - keep nextTag var defined in build_tag.sh

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 nextTag="" # will be populated in build_tag.sh
-bash "prod/build_tag.sh"
+source "prod/build_tag.sh"
+echo "next tag from build_tag.sh: $nextTag"
 if [ -z "$nextTag" ]; then
     echo "build_tag.sh did not populate the nextTag var"
     exit 1;
