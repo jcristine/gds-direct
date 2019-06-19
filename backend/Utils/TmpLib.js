f429650d4ebb7600203638086ad26542c009e1aa
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-06-19 15:21:33 +0000

    Update klesun-node-tools for the Lang.coverExc()

--- backend/Utils/TmpLib.js
+++ backend/Utils/TmpLib.js
@@ -1,5 +1,6 @@
 
 const GrectLib = require('klesun-node-tools');
+const Lang = require("klesun-node-tools/src/Lang");
 
 // TODO: refactor code to use it directly from the lib
 exports.hrtimeToDecimal = GrectLib.Misc.hrtimeToDecimal;
@@ -35,24 +36,8 @@ exports.safe = getter => {
 
 exports.wrapExc = async getter => getter();
 
-/**
- * @template T
- * @param {T} defaultValue
- * @param {{httpStatusCode}[]} allowedKinds - Rej.* http status codes
- * @return {function(Error): Promise<T>} - resolve if exc was created with Rej.* and it's
- *           status is in allowedStatuses, otherwise reject with original exc
- * supposed to be used with promise.catch(coverExc(null, [Rej.NotFound])) to catch particular kinds of exceptions
- */
-exports.coverExc = (defaultValue, allowedKinds) => {
-	return (exc) => {
-		let allowedCodes = allowedKinds.map(r => r.httpStatusCode);
-		if (exc && allowedCodes.includes(exc.httpStatusCode)) {
-			return Promise.resolve(defaultValue);
-		} else {
-			return Promise.reject(exc);
-		}
-	};
-};
+/** @deprecated - use from Lang.js module from the lib instead */
+exports.coverExc = Lang.coverExc;
 
 exports.addPerformanceDebug = (label, prevResult = null) => {
 	let startMs = Date.now();
