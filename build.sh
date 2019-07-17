3553c497cdfd5038a3c1031125504169e6441752
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-07-17 14:17:35 +0000

    Remove outdated comments from build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# less should be installed globally - npm install less -g
-# lessc public/_style/less/main.less > public/_style/css/parsed.css
-
-# downloads /node_modules/ if needed
 npm ci || exit 1;
 
 startpath=$(pwd)
