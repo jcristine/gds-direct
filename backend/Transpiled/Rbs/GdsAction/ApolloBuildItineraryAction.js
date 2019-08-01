875cd7fd51dc564073a9f75ce0ce6bd84850c955
Dainis Tillers <d.tillers@dynatech.lv>
2019-08-01 09:56:56 +0300

    GDSD-63: Add ability to use xml when adding pnr

--- backend/Transpiled/Rbs/GdsAction/ApolloBuildItineraryAction.js
+++ backend/Transpiled/Rbs/GdsAction/ApolloBuildItineraryAction.js
@@ -1,18 +1,31 @@
-
 const StringUtil = require('../../Lib/Utils/StringUtil.js');
+const DateTime = require('../../../Transpiled/Lib/Utils/DateTime');
 const ItineraryParser = require('../../Gds/Parsers/Apollo/Pnr/ItineraryParser.js');
 const AbstractGdsAction = require('./AbstractGdsAction.js');
 const AirAvailabilityParser = require("../../Gds/Parsers/Apollo/AirAvailabilityParser");
 const {fetchAll} = require("../../../GdsHelpers/TravelportUtils.js");
 const {REBUILD_NO_AVAIL, REBUILD_GDS_ERROR} = require('../GdsDirect/Errors.js');
+const TravelportClient = require("../../../GdsClients/TravelportClient");
+const _ = require("lodash");
+const moment = require("moment");
 
 let php = require('../../phpDeprecated.js');
 
 class ApolloBuildItineraryAction extends AbstractGdsAction {
+	constructor() {
+		super();
+		this.usesXml = true;
+	}
+
+	useXml(flag) {
+		this.usesXml = flag;
+		return this;
+	}
 
 	async executeViaTerminal($itinerary, $isParserFormat) {
 		let $segmentsSold, $i, $segment, $cmd, $output, $errorType, $tplData;
 		$segmentsSold = 0;
+
 		for ([$i, $segment] of Object.entries($itinerary)) {
 			$cmd = this.constructor.makeDirectSellCmd($segment, $isParserFormat);
 			$output = (await fetchAll($cmd, this.session)).output;
@@ -38,12 +51,67 @@ class ApolloBuildItineraryAction extends AbstractGdsAction {
 					'success': false,
 					'segmentsSold': $segmentsSold,
 					'errorType': $errorType,
-					'errorData': $tplData
+					'errorData': $tplData,
 				};
 			}
 			++$segmentsSold;
 		}
 		return {'success': true, 'segmentsSold': $segmentsSold};
+	}
+
+	async executeViaXml(itinerary, isParserFormat = false) {
+		let soldCount = 0;
+
+		const byStatus = _.groupBy(itinerary, e => e.segmentStatus);
+		const startDate = this.session.getStartDt() || moment().format("YYYY-MM-DD");
+
+		// Travelport returns SYSTEM ERROR if you book GK and SS segments at same time
+		for(const segment of Object.values(byStatus)) {
+			// Travelport returns SYSTEM ERROR if you book more than 8 segments at same time
+			for(const chunk of _.chunk(segment, 8)) {
+				const airSegments = chunk.map(segment => ({
+					airline: segment.airline,
+					flightNumber: segment.flightNumber,
+					bookingClass: segment.bookingClass,
+					departureDt: !isParserFormat ?
+						segment.departureDate : DateTime.decodeRelativeDateInFuture(segment.departureDate.parsed, startDate),
+					destinationDt: null,
+					departureAirport: segment.departureAirport,
+					destinationAirport: segment.destinationAirport,
+					segmentStatus: segment.segmentStatus,
+					seatCount: segment.seatCount,
+				}));
+
+				const params = {
+					addAirSegments: airSegments,
+				};
+
+				try {
+					this.session.logit("Will add pnr", {chunk, params});
+
+					const result = await TravelportClient.processPnr(this.session.getGdsData(), params);
+
+					this.session.logit("PNR added", result);
+
+					soldCount += result.newAirSegments.filter(seg => seg.success).length;
+				} catch(error) {
+					this.session.logit("Failed to add pnr", {
+						params, error,
+					});
+
+					return {
+						success: false,
+						segmentsSold: soldCount,
+						errorType: ApolloBuildItineraryAction.ERROR_MULTISEGMENT,
+						errorData: error.message,
+					};
+				}
+			}
+		}
+
+		this.session.logit("All PNRs added", {itinerary, soldCount});
+
+		return {success: true, segmentsSold: soldCount};
 	}
 
 	static makeDirectSellCmd($segment, $isParserFormat) {
@@ -83,7 +151,11 @@ class ApolloBuildItineraryAction extends AbstractGdsAction {
 	}
 
 	async execute($itinerary, $isParserFormat) {
-		return this.executeViaTerminal($itinerary, $isParserFormat);
+		if(this.usesXml) {
+			return this.executeViaXml($itinerary, $isParserFormat);
+		} else {
+			return this.executeViaTerminal($itinerary, $isParserFormat);
+		}
 	}
 }
 
