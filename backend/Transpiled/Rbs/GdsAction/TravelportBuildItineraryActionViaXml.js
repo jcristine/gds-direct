0c4df3b3ca158ef1f115a83b7c6b95c88f492c5b
Dainis Tillers <d.tillers@dynatech.lv>
2019-08-12 17:27:06 +0300

    GDSD-63 Separate travelport soap itinerary build in separate function

---
+++ backend/Transpiled/Rbs/GdsAction/TravelportBuildItineraryActionViaXml.js
@@ -0,0 +1,60 @@
+const DateTime = require('../../../Transpiled/Lib/Utils/DateTime');
+const _ = require('lodash');
+const moment = require('moment');
+const TravelportClient = require('../../../GdsClients/TravelportClient');
+const {REBUILD_MULTISEGMENT} = require('../GdsDirect/Errors');
+
+// Used by both Galileo and Apollo itinary build actions
+// They use same uderlaying API, the only difference is slight
+// changes in format data is returned
+const TravelportBuildItineraryViaXml = async ({
+	session, itinerary,
+	baseDate = moment().format('YYYY-MM-DD'),
+	travelport = TravelportClient()
+}) => {
+	const segments = [];
+	const byStatus = _.groupBy(itinerary, e => e.segmentStatus);
+	const startDate = baseDate;
+	let reservation = null;
+
+	// Travelport returns SYSTEM ERROR if you book GK and SS segments at same time
+	for(const segment of Object.values(byStatus)) {
+		// Travelport returns SYSTEM ERROR if you book more than 8 segments at same time
+		for(const chunk of _.chunk(segment, 8)) {
+			const airSegments = chunk.map(segment => ({
+				airline: segment.airline,
+				flightNumber: segment.flightNumber,
+				bookingClass: segment.bookingClass,
+				departureDt: DateTime.decodeRelativeDateInFuture(segment.departureDate.parsed, startDate),
+				destinationDt: null,
+				departureAirport: segment.departureAirport,
+				destinationAirport: segment.destinationAirport,
+				segmentStatus: segment.segmentStatus,
+				seatCount: segment.seatCount,
+			}));
+
+			const params = {
+				addAirSegments: airSegments,
+			};
+
+			const result = await travelport.processPnr(session.getGdsData(), params);
+
+			segments.push(...result.newAirSegments.filter(seg => seg.success));
+
+			if (result.error) {
+				return {
+					success: false,
+					errorType: REBUILD_MULTISEGMENT,
+					errorData: {response: result.error},
+					segments,
+				};
+			}
+
+			reservation = result.reservation;
+		}
+	}
+
+	return {success: true, segments, reservation};
+};
+
+module.exports = TravelportBuildItineraryViaXml;
