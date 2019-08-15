44384445842995c7ec121551697c6b9fa5ea9150
Dainis Tillers <d.tillers@dynatech.lv>
2019-08-15 15:52:25 +0300

    GDSD-85 Add sabre to fixture list

--- backend/Utils/Testing/GdsActionTestUtil.js
+++ backend/Utils/Testing/GdsActionTestUtil.js
@@ -30,15 +30,21 @@ exports.testHttpGdsAction = async ({unit, testCase, getActual}) => {
 	let {input, fullState, output, httpRequests, startDt = undefined} = testCase;
 	let PersistentHttpRq = PersistentHttpRqStub(httpRequests);
 	let stubGdsProfiles = {
-		getTravelport: (profileName) => Promise.resolve({
+		getTravelport: profileName => Promise.resolve({
 			username: 'grectUnitTest',
 			password: 'qwerty123',
 		}),
-		getAmadeus: (profileName) => Promise.resolve({
+		getAmadeus: profileName => Promise.resolve({
 			endpoint: 'https://nodeD1.production.webservices.amadeus.com/1ASIWTUTICO',
 			username: 'grectUnitTest',
 			password: 'qwerty123',
 			default_pcc: 'SFO1S2195',
+		}),
+		getSabre: profileName => Promise.resolve({
+			password: 'somePassword',
+			username: 'someUserName',
+			default_pcc: 'somePCC',
+			profileName,
 		}),
 		chooseAmaProfile: GdsProfiles.chooseAmaProfile,
 	};
@@ -46,6 +52,9 @@ exports.testHttpGdsAction = async ({unit, testCase, getActual}) => {
 		context: {gds: fullState.gds, travelRequestId: null},
 		gdsData: {
 			sessionToken: 'soap-unit-test-blabla-123',
+			binarySecurityToken: 'soap-unit-test', // for sabre
+			conversationId: 777,
+			messageId: 666,
 		},
 	};
 	let gdsClients = GdsSession.makeGdsClients({
