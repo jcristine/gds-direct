5dd6d187edb420f40ae97a4ececa4f9995fd0e3b
Dainis Tillers <d.tillers@dynatech.lv>
2019-08-15 15:53:06 +0300

    GDSD-85 Strip all unecessary whitespaces when matching request bodies making fixture creation easier

--- backend/Utils/Testing/PersistentHttpRqStub.js
+++ backend/Utils/Testing/PersistentHttpRqStub.js
@@ -8,12 +8,12 @@
  *
  * @param {{rq: string, rs: string}[]} httpRequests
  */
-const PersistentHttpRqStub = (httpRequests) => {
+const PersistentHttpRqStub = httpRequests => {
 	let PersistentHttpRq = ({url, headers, body}) => {
 		let nextResult = httpRequests.shift();
 		if (!nextResult) {
 			throw new Error('Tried to make http request when all stub values were exhausted: ' + url + '\n' + body);
-		} else if (body !== nextResult.rq) {
+		} else if (normalizeBody(body) !== normalizeBody(nextResult.rq)) {
 			throw new Error('Tried to make unexpected http request ' + url + '.\nExpected:\n' + nextResult.rq + '\nActual:\n' + body);
 		} else {
 			return Promise.resolve({
@@ -25,5 +25,13 @@ const PersistentHttpRqStub = (httpRequests) => {
 	PersistentHttpRq.getHttpRequestsLeft = () => httpRequests;
 	return PersistentHttpRq;
 };
+
+// Remove all white spaces that are in the request body without affecting structure
+// so that you don't have to construct exactly same string to match the fixture
+const normalizeBody = body => body
+	.replace(/>\s+</gm, '><')
+	.replace(/^\s+/gm, '')
+	.replace(/\s+$/gm, '')
+	.replace(/([a-zA-Z0-9:]+={0,1}("[^\s]+"){0,1})\s+/gm, '$1 '); //for atribute values
 
 module.exports = PersistentHttpRqStub;
