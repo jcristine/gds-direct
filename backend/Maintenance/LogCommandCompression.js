762c4e5c82d164100cf14ec90123d46a5b904e2b
Dainis Tillers <d.tillers@dynatech.lv>
2019-08-22 12:53:44 +0300

    GDSD-87 Add migration scripts that create required tables

--- backend/Maintenance/LogCommandCompression.js
+++ backend/Maintenance/LogCommandCompression.js
@@ -1,15 +1,7 @@
 
 const schedule = require('node-schedule');
-const TicketDesignators = require("../Repositories/TicketDesignators");
-const BookingClasses = require("../Repositories/BookingClasses");
-const Airlines = require("../Repositories/Airlines");
-const Pccs = require("../Repositories/Pccs");
-const Airports = require("../Repositories/Airports");
 const FluentLogger = require("../LibWrappers/FluentLogger");
 const Redis = require("../LibWrappers/Redis");
-const Agents = require("../Repositories/Agents");
-const CmdRsLog = require("../Repositories/CmdRsLog.js");
-const PtcFareTypes = require('../Repositories/PtcFareFamilies.js');
 const {descrProc} = require('../Utils/Clustering.js');
 const {coverExc} = require('klesun-node-tools/src/Lang.js');
 const Rej = require('klesun-node-tools/src/Rej.js');
