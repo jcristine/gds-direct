1395735d862bdfa542d92fbeefe328f91e48dbe9
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-08-26 18:14:10 +0000

    Remove php var name artifacts in GalileoPricingAdapter.js

--- backend/Transpiled/Rbs/FormatAdapters/GalileoPricingAdapter.js
+++ backend/Transpiled/Rbs/FormatAdapters/GalileoPricingAdapter.js
@@ -21,32 +21,30 @@ class GalileoPricingAdapter {
 
 	getMods() {
 		return !this.$pricingCommand ? [] :
-			((CommandParser.parse(this.$pricingCommand) || {})['data'] || {})['pricingModifiers'] || [];
+			((CommandParser.parse(this.$pricingCommand) || {}).data || {}).pricingModifiers || [];
 	}
 
-	static getFirst($predicate, $iterable, $fallback) {
-		for (const $element of Object.values($iterable)) {
-			if ($predicate($element)) {
-				return $element;
+	static getFirst(predicate, iterable, fallback) {
+		for (const element of Object.values(iterable)) {
+			if (predicate(element)) {
+				return element;
 			}
 		}
-		return $fallback;
+		return fallback;
 	}
 
 	getModPtcData($passengerNumber) {
-		let $hasPassengerNumber, $mods, $modTypeToData, $pMod;
+		const hasPassengerNumber = ($group) => php.in_array($passengerNumber, $group.passengerNumbers);
 
-		$hasPassengerNumber = ($group) => php.in_array($passengerNumber, $group['passengerNumbers']);
-
-		$mods = this.getMods();
-		$modTypeToData = php.array_combine(
-			php.array_column($mods, 'type'),
-			php.array_column($mods, 'parsed')
+		const mods = this.getMods();
+		const modTypeToData = php.array_combine(
+			php.array_column(mods, 'type'),
+			php.array_column(mods, 'parsed')
 		);
-		$pMod = $modTypeToData['passengers'];
-		return $pMod && $pMod['appliesToAll']
-			? $pMod['ptcGroups'][0]
-			: this.constructor.getFirst($hasPassengerNumber, ($pMod || {})['ptcGroups'] || [], null);
+		const pMod = modTypeToData.passengers;
+		return pMod && pMod.appliesToAll
+			? pMod.ptcGroups[0]
+			: this.constructor.getFirst(hasPassengerNumber, (pMod || {}).ptcGroups || [], null);
 	}
 
 	static transformFareInfo($ptcBlock) {
@@ -59,112 +57,108 @@ class GalileoPricingAdapter {
 		};
 	}
 
-	static transformSegment($segment) {
-		let $segmentDetails;
-
-		$segmentDetails = $segment['segmentDetails'];
-		$segment['segmentDetails']['bagWithoutFeeNumber'] = ($segmentDetails['freeBaggageAmount'] || {})['raw'];
-		$segment['segmentDetails']['bagWithoutFeeNumberParsed'] = ($segmentDetails['freeBaggageAmount'] || {})['parsed'];
-		delete ($segment['segmentDetails']['freeBaggageAmount']);
-		return $segment;
+	static transformSegment(segment) {
+		const segmentDetails = segment.segmentDetails;
+		segment.segmentDetails.bagWithoutFeeNumber = (segmentDetails.freeBaggageAmount || {}).raw;
+		segment.segmentDetails.bagWithoutFeeNumberParsed = (segmentDetails.freeBaggageAmount || {}).parsed;
+		delete segment.segmentDetails.freeBaggageAmount;
+		return segment;
 	}
 
-	static transformBaggageInfo($baggage) {
-		if (php.empty($baggage)) {
+	static transformBaggageInfo(baggage) {
+		if (php.empty(baggage)) {
 			return null;
 		}
 		return {
-			raw: $baggage['raw'],
-			parsed: php.empty($baggage['parsed']) ? null : {
+			raw: baggage.raw,
+			parsed: php.empty(baggage.parsed) ? null : {
 				baggageAllowanceBlocks: Fp.map(($block) => ({
-					paxTypeCode: $block['paxTypeCode'],
+					paxTypeCode: $block.paxTypeCode,
 					segments: php.array_map((...args) => this.transformSegment(...args), $block['segments']),
-				}), ($baggage['parsed'] || {})['baggageAllowanceBlocks'] || []),
+				}), (baggage.parsed || {}).baggageAllowanceBlocks || []),
 				carryOnAllowanceBlock: {
 					segments: php.array_map((...args) => this.transformSegment(...args),
-						(($baggage['parsed'] || {})['carryOnAllowanceBlock'] || {})['segments'] || []),
+						((baggage['parsed'] || {})['carryOnAllowanceBlock'] || {})['segments'] || []),
 				},
 			},
 		};
 	}
 
-	transformPtcBlock($ptcBlock, $ptcMessages, $baggageBlock, $paxNumber) {
-		let $typeToMsgData, $mods, $modTypeToData, $modPtcData, $cmdPtc;
-
-		$typeToMsgData = php.array_combine(
-			php.array_column($ptcMessages, 'type'),
-			php.array_column($ptcMessages, 'parsed')
+	transformPtcBlock(ptcBlock, ptcMessages, baggageBlock, paxNumber) {
+		const typeToMsgData = php.array_combine(
+			php.array_column(ptcMessages, 'type'),
+			php.array_column(ptcMessages, 'parsed')
 		);
-		$mods = this.getMods();
-		$modTypeToData = php.array_combine(
-			php.array_column($mods, 'type'),
-			php.array_column($mods, 'parsed')
+		const mods = this.getMods();
+		const modTypeToData = php.array_combine(
+			php.array_column(mods, 'type'),
+			php.array_column(mods, 'parsed')
 		);
-		$modPtcData = this.getModPtcData($paxNumber);
-		$cmdPtc = ($modPtcData || {})['ptc'] || $ptcBlock['ptc'];
+		const modPtcData = this.getModPtcData(paxNumber);
+		const cmdPtc = (modPtcData || {}).ptc || ptcBlock.ptc;
 		return {
 			ptcInfo: {
-				ptc: $ptcBlock['ptc'],
-				ageGroup: PtcUtil.parsePtc($ptcBlock['ptc'])['ageGroup'],
-				ptcRequested: $cmdPtc,
-				ageGroupRequested: PtcUtil.parsePtc($cmdPtc)['ageGroup'],
-				quantity: php.count($ptcBlock['passengerNumbers']),
+				ptc: ptcBlock.ptc,
+				ageGroup: PtcUtil.parsePtc(ptcBlock.ptc).ageGroup,
+				ptcRequested: cmdPtc,
+				ageGroupRequested: PtcUtil.parsePtc(cmdPtc).ageGroup,
+				quantity: php.count(ptcBlock.passengerNumbers),
 			},
-			// I can't believe there actually is a GDS that
-			// uses only absolute passenger numbers...
-			passengerNameNumbers: Fp.map(($abs) => ({
-				absolute: $abs,
-				fieldNumber: $abs,
+			// there does not seem to be a format to price paxes stored
+			// on same field number separately in Galileo unlike Apollo
+			passengerNameNumbers: ptcBlock.passengerNumbers.map(abs => ({
+				absolute: abs,
+				fieldNumber: abs,
 				firstNameNumber: 1,
-			}), $ptcBlock['passengerNumbers']),
-			validatingCarrier: $typeToMsgData['defaultPlatingCarrier'] || $modTypeToData['validatingCarrier'] || $modTypeToData['overrideCarrier'],
-			hasPrivateFaresSelectedMessage: $typeToMsgData['hasPrivateFaresSelectedMessage'] || false,
-			lastDateToPurchase: !php.isset($typeToMsgData['lastDateToPurchase']) ? null : {
-				full: $typeToMsgData['lastDateToPurchase']['parsed'],
+			})),
+			validatingCarrier:
+				typeToMsgData.defaultPlatingCarrier ||
+				modTypeToData.validatingCarrier ||
+				modTypeToData.overrideCarrier,
+			hasPrivateFaresSelectedMessage: typeToMsgData.hasPrivateFaresSelectedMessage || false,
+			lastDateToPurchase: !php.isset(typeToMsgData.lastDateToPurchase) ? null : {
+				full: typeToMsgData.lastDateToPurchase.parsed,
 			},
 			// requires separate command - >FQL{ptcNumber}; for each PTC group
 			endorsementBoxLines: [],
 			privateFareType: null,
 			tourCode: null,
-			fareInfo: this.constructor.transformFareInfo($ptcBlock),
+			fareInfo: this.constructor.transformFareInfo(ptcBlock),
 
-			baggageInfo: this.constructor.transformBaggageInfo($baggageBlock),
+			baggageInfo: this.constructor.transformBaggageInfo(baggageBlock),
 		};
 	}
 
 	/**
-	 * @param $ptcList = FqParser::parse()
-	 * @param $linearFare = Galileo\Pricing\LinearFareParser::parse()
+	 * @param ptcList = require('FqParser.js').parse()
+	 * @param linearFare = Galileo\Pricing\LinearFareParser::parse()
 	 */
-	transform($ptcList, $linearFare) {
-		let $getType, $typeToPtcMsgs, $taPccs, $getFullPtc, $fullPtcToMsgs, $pricingBlockList, $i, $ptcBlock,
-			$paxNumber, $modPtcData, $fullPtc, $msgs, $baggageBlock;
-
+	transform(ptcList, linearFare) {
 		if (!this.$pricingCommand) {
-			this.setPricingCommand($ptcList['cmdCopy']);
+			this.setPricingCommand(ptcList.cmdCopy);
 		}
 
-		$getType = ($msgRec) => $msgRec['type'];
-		$typeToPtcMsgs = Fp.groupBy($getType, $ptcList['ptcMessages'] || []);
-		$taPccs = php.array_unique(php.array_column($typeToPtcMsgs['ticketingAgencyPcc'] || [], 'parsed'));
+		const getType = msgRec => msgRec.type;
+		const typeToPtcMsgs = Fp.groupBy(getType, ptcList.ptcMessages || []);
+		const taPccs = php.array_unique(php.array_column(typeToPtcMsgs.ticketingAgencyPcc || [], 'parsed'));
 
-		$getFullPtc = ($msgRec) => $msgRec['ptc'] + $msgRec['ptcDescription'];
-		$fullPtcToMsgs = Fp.groupBy($getFullPtc, $ptcList['ptcMessages'] || []);
+		const getFullPtc = msgRec => msgRec.ptc + msgRec.ptcDescription;
+		const fullPtcToMsgs = Fp.groupBy(getFullPtc, ptcList.ptcMessages || []);
 
-		$pricingBlockList = [];
-		for ([$i, $ptcBlock] of Object.entries($linearFare['ptcBlocks'])) {
-			$paxNumber = ArrayUtil.getFirst($ptcBlock['passengerNumbers']);
-			$modPtcData = this.getModPtcData($paxNumber);
-			$fullPtc = $modPtcData && $modPtcData['ptc'] ? $modPtcData['ptc'] + $modPtcData['ptcDescription'] : 'ADT';
-			$msgs = $fullPtcToMsgs[$fullPtc] || [];
-			$baggageBlock = ($ptcList['bagPtcPricingBlocks'] || {})[$i];
-			$pricingBlockList.push(this.transformPtcBlock($ptcBlock, $msgs, $baggageBlock, $paxNumber));
+		const pricingBlockList = [];
+		for (const [i, ptcBlock] of Object.entries(linearFare.ptcBlocks)) {
+			const paxNumber = ArrayUtil.getFirst(ptcBlock['passengerNumbers']);
+			const modPtcData = this.getModPtcData(paxNumber);
+			const fullPtc = modPtcData && modPtcData.ptc ? modPtcData.ptc + modPtcData.ptcDescription : 'ADT';
+			const msgs = fullPtcToMsgs[fullPtc] || [];
+			const baggageBlock = (ptcList.bagPtcPricingBlocks || {})[i];
+			pricingBlockList.push(this.transformPtcBlock(ptcBlock, msgs, baggageBlock, paxNumber));
 		}
 		return {
 			quoteNumber: null,
-			pricingPcc: php.count($taPccs) === 1 ? $taPccs[0] : null,
+			pricingPcc: php.count(taPccs) === 1 ? taPccs[0] : null,
 			pricingModifiers: this.getMods(),
-			pricingBlockList: $pricingBlockList,
+			pricingBlockList: pricingBlockList,
 		};
 	}
 }
