8d21879e15480fa1a90e7973e9c087683db050f7
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-08-27 20:00:25 +0000

    Add partial implementation of BookViaGk_amadeus.js

--- backend/Actions/BookViaGk/BookViaGk.js
+++ backend/Actions/BookViaGk/BookViaGk.js
@@ -1,23 +1,8 @@
+const BookViaGk_amadeus = require('./BookViaGk_amadeus.js');
 const BookViaGk_sabre = require('./BookViaGk_sabre.js');
 const BookViaAk_galileo = require('./BookViaAk_galileo.js');
 const BookViaGk_apollo = require('./BookViaGk_apollo.js');
-const AmadeusBuildItineraryAction = require('../../Transpiled/Rbs/GdsAction/AmadeusBuildItineraryAction.js');
-const Rej = require('klesun-node-tools/src/Rej.js');
 const GdsSession = require('../../GdsHelpers/GdsSession.js');
-
-const inAmadeus = async ({
-	bookRealSegments = false,
-	itinerary, ...bookParams
-}) => {
-	itinerary = itinerary.map(seg => ({...seg, segmentStatus: 'GK'}));
-	const built = await new AmadeusBuildItineraryAction(bookParams).execute(itinerary);
-	if (built.errorType) {
-		return Rej.UnprocessableEntity('Could not rebuild PNR in Amadeus - '
-			+ built.errorType + ' ' + JSON.stringify(built.errorData));
-	} else {
-		return Promise.resolve({reservation: built.reservation});
-	}
-};
 
 /**
  * unlike ApolloBuildItineraryAction.js, this one also minds the marriage
@@ -35,11 +20,9 @@ const BookViaGk = ({
 	return {
 		apollo: () => BookViaGk_apollo({...params, travelport}),
 		galileo: () => BookViaAk_galileo({...params, travelport}),
-		amadeus: () => inAmadeus({...params, amadeus}),
+		amadeus: () => BookViaGk_amadeus({...params, amadeus}),
 		sabre: () => BookViaGk_sabre({...params, sabre}),
 	}[gds]();
 };
-
-BookViaGk.inAmadeus = inAmadeus;
 
 module.exports = BookViaGk;
