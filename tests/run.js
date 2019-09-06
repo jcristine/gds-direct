4d0f68cb692c44c929b450474d956e49b239e111
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-09-06 12:22:44 +0000

    Update klesun-node-tools - to drop Dyn bundled with it

--- tests/run.js
+++ tests/run.js
@@ -2,6 +2,7 @@
 require('../backend/Utils/Polyfills.js');
 require('./disableSideEffects.js');
 
+const {getConfig, getDbConfig} = require('dyn-utils/src/Config.js');
 const Config = require('../backend/Config.js');
 const RunTests = require('klesun-node-tools/src/Transpiled/RunTests.js');
 const {timeout} = require('klesun-node-tools/src/Lang.js');
@@ -14,6 +15,18 @@ const main = async () => {
 	(await Config.getConfig().catch(exc => ({}))).external_service = {};
 
 	console.log('Starting unit tests');
+
+	Object.assign(await getDbConfig().catch(exc => ({})), {
+		DB_NAME: null,
+		DB_USER: null,
+		DB_PASS: null,
+		DB_HOST: null,
+		DB_PORT: null,
+	}).DB_NAME = null;
+	const config = await getConfig().catch(exc => ({}));
+	config.external_service = {};
+	config.RBS_PASSWORD = null;
+
 	return RunTests({
 		rootPath: __dirname + '/backend/',
 		ignoredPaths: [
