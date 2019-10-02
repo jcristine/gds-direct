a7ed6948c27fec03e374b52b6db979524f4537ab
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-10-02 14:39:45 +0000

    Add filtering to MP admin page

--- public/admin/mpLog.js
+++ public/admin/mpLog.js
@@ -7,18 +7,47 @@ const Cmp = (...args) => new Component(...args);
 const whenEmcSessionId = window.GdsDirectPlusPage.whenEmcSessionId;
 const grectApi = GrectApi({whenEmcSessionId});
 
+const matchesFilters = (filters, record) => {
+	const minDate = filters.minDate.value;
+	const maxDate = filters.maxDate.value;
+	const company = filters.company.value;
+	const mpAirline = filters.mpAirline.value;
+	const agentLogin = filters.agentLogin.value;
+	const mpPcc = filters.mpPcc.value;
+	return (!minDate || record.dt.slice(0, 10) >= minDate)
+		&& (!minDate || record.dt.slice(0, 10) <= maxDate)
+		&& (!company || record.agentCompanies.includes(company))
+		&& (!mpAirline || record.mpAirline === mpAirline)
+		&& (!agentLogin || record.agentLogin === agentLogin)
+		&& (!mpPcc || record.mpPcc === mpPcc)
+	;
+};
+
 const main = () => {
 	const mpList = document.getElementById('mp-action-list');
+	const filterForm = document.forms[0];
+	const filters = filterForm.elements;
 	grectApi.admin.getMpLog().then(({records}) => {
-		for (const record of records) {
-			mpList.appendChild(Cmp('tr').attach([
-				Cmp('td', {textContent: record.dt}),
-				Cmp('td', {textContent: record.agentLogin}),
-				Cmp('td', {textContent: record.agentCompanies.join(', ')}),
-				Cmp('td', {textContent: record.mpAirline}),
-				Cmp('td', {textContent: record.mpPcc}),
-			]).context);
-		}
+		const render = () => {
+			mpList.textContent = '';
+			for (const record of records) {
+				if (matchesFilters(filters, record)) {
+					mpList.appendChild(Cmp('tr').attach([
+						Cmp('td', {textContent: record.dt}),
+						Cmp('td', {textContent: record.agentLogin}),
+						Cmp('td', {textContent: record.agentCompanies.join(', ')}),
+						Cmp('td', {textContent: record.mpAirline}),
+						Cmp('td', {textContent: record.mpPcc}),
+					]).context);
+				}
+			}
+		};
+		filterForm.onsubmit = (evt) => {
+			evt.preventDefault();
+			render();
+			return false;
+		};
+		render();
 	});
 };
 
