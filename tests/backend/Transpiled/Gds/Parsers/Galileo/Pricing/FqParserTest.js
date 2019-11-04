d5d08a17ae612caf4046fd9437b28b533516627e
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-11-04 20:41:51 +0000

    Reuse part of existing Galileo FQ output parser code in the lib

--- tests/backend/Transpiled/Gds/Parsers/Galileo/Pricing/FqParserTest.js
+++ tests/backend/Transpiled/Gds/Parsers/Galileo/Pricing/FqParserTest.js
@@ -7,12 +7,12 @@ const php = require('klesun-node-tools/src/Transpiled/php.js');
 class FqParserTest extends require('klesun-node-tools/src/Transpiled/Lib/TestCase.js')
 {
 	provideTestCases()  {
-		let $list;
+		let list;
 
-		$list = [];
+		list = [];
 
 		// first dump I found
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				'>FQBB',
 				'                   *** BEST BUY QUOTATION ***',
@@ -125,7 +125,7 @@ class FqParserTest extends require('klesun-node-tools/src/Transpiled/Lib/TestCas
 		]);
 
 		// with 2 PTC-s
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				'>FQP1*ADT.2*C05',
 				'   PSGR                  FARE     TAXES         TOTAL PSG DES   ',
@@ -313,7 +313,7 @@ class FqParserTest extends require('klesun-node-tools/src/Transpiled/Lib/TestCas
 		]);
 
 		// 3 PTC-s, marked fare basis  and rebook is required
-		$list.push([
+		list.push([
 			php.implode(php.PHP_EOL, [
 				'>FQBB',
 				'                   *** BEST BUY QUOTATION ***',
@@ -872,314 +872,7 @@ class FqParserTest extends require('klesun-node-tools/src/Transpiled/Lib/TestCas
 			},
 		]);
 
-		$list.push([
-			php.implode(php.PHP_EOL, [
-				'>FQP1*C.2*INF',
-				'   PSGR                  FARE     TAXES         TOTAL PSG DES   ',
-				'FQG 1         USD      627.00      80.50       707.50 ADT       ',
-				'    GUARANTEED AT TIME OF TICKETING                             ',
-				'FQG 2         USD       63.00                   63.00 INF       ',
-				'    GUARANTEED AT TIME OF TICKETING                             ',
-				'GRAND TOTAL INCLUDING TAXES ****     USD       770.50           ',
-				'             **ADDITIONAL FEES MAY APPLY**SEE >FO; ',
-				'       **CARRIER MAY OFFER ADDITIONAL SERVICES**SEE >FQ/DASO;',
-				'    C        BEST FARE FOR THIS PASSENGER                       ',
-				'    C        RATE USED IN EQU TOTAL IS BSR 1EUR - 1.230293USD   ',
-				'    C        LAST DATE TO PURCHASE TICKET: 03MAY18              ',
-				'    C        TICKETING AGENCY 711M                              ',
-				'    C        DEFAULT PLATING CARRIER PS                         ',
-				'    C        E-TKT REQUIRED                                     ',
-				'    INF      RATE USED IN EQU TOTAL IS BSR 1EUR - 1.230293USD   ',
-				'    INF      LAST DATE TO PURCHASE TICKET: 03MAY18              ',
-				'    INF      TICKETING AGENCY 711M                              ',
-				'    INF      DEFAULT PLATING CARRIER PS                         ',
-				'    INF      E-TKT REQUIRED                                     ',
-				'BAGGAGE ALLOWANCE',
-				'CNN',
-				' PS KIVRIX  2PC  ',
-				'   BAG 1 -  NO FEE       UPTO70LB/32KG AND UPTO62LI/158LCM ',
-				'   BAG 2 -  NO FEE       UPTO70LB/32KG AND UPTO62LI/158LCM ',
-				'   MYTRIPANDMORE.COM/BAGGAGEDETAILSPS.BAGG             ',
-				'                                                               ',
-				'CARRY ON ALLOWANCE',
-				' PS KIVIEV  12K   ',
-				'   BAG 1 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'   BAG 2 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				' PS IEVRIX  12K   ',
-				'   BAG 1 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'   BAG 2 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'INF',
-				' PS KIVRIX  1PC  ',
-				'   BAG 1 -  NO FEE       UPTO22LB/10KG AND UPTO45LI/115LCM ',
-				'   BAG 2 -  BAGGAGE CHARGES DATA NOT AVAILABLE            ',
-				'   MYTRIPANDMORE.COM/BAGGAGEDETAILSPS.BAGG             ',
-				'                                                               ',
-				'CARRY ON ALLOWANCE',
-				' PS KIVIEV  05K   ',
-				'   BAG 1 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'   BAG 2 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				' PS IEVRIX  05K   ',
-				'   BAG 1 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'   BAG 2 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'                                                               ',
-				'BAGGAGE DISCOUNTS MAY APPLY BASED ON FREQUENT FLYER STATUS/',
-				'ONLINE CHECKIN/FORM OF PAYMENT/MILITARY/ETC.',
-			]),
-			{
-				'cmdCopy': 'FQP1*C.2*INF',
-				'ptcRows': [
-					{'passengerNumbers': [1], 'ptc': 'ADT'},
-					{'passengerNumbers': [2], 'ptc': 'INF'},
-				],
-				'grandTotal': {'currency': 'USD','amount': '770.50'},
-				'ptcMessages': [
-					{'ptc': 'C', 'raw': 'BEST FARE FOR THIS PASSENGER                       '},
-					{'ptc': 'C', 'raw': 'RATE USED IN EQU TOTAL IS BSR 1EUR - 1.230293USD   '},
-					{'ptc': 'C', 'raw': 'LAST DATE TO PURCHASE TICKET: 03MAY18              '},
-					{'ptc': 'C', 'raw': 'TICKETING AGENCY 711M                              '},
-					{'ptc': 'C', 'raw': 'DEFAULT PLATING CARRIER PS                         '},
-					{'ptc': 'C', 'raw': 'E-TKT REQUIRED                                     '},
-
-					{'ptc': 'INF', 'raw': 'RATE USED IN EQU TOTAL IS BSR 1EUR - 1.230293USD   '},
-					{'ptc': 'INF', 'raw': 'LAST DATE TO PURCHASE TICKET: 03MAY18              '},
-					{'ptc': 'INF', 'raw': 'TICKETING AGENCY 711M                              '},
-					{'ptc': 'INF', 'raw': 'DEFAULT PLATING CARRIER PS                         '},
-					{'ptc': 'INF', 'raw': 'E-TKT REQUIRED                                     '},
-				],
-			},
-		]);
-
-		$list.push([
-			php.implode(php.PHP_EOL, [
-				'>FQP1*SRC65LGB.2*SRC75LGB',
-				'   PSGR                  FARE     TAXES         TOTAL PSG DES   ',
-				'FQG 1         USD      627.00      80.50       707.50 ADT       ',
-				'    GUARANTEED AT TIME OF TICKETING                             ',
-				'FQG 2         USD      627.00      80.50       707.50 ADT       ',
-				'    GUARANTEED AT TIME OF TICKETING                             ',
-				'GRAND TOTAL INCLUDING TAXES ****     USD      1415.00           ',
-				'             **ADDITIONAL FEES MAY APPLY**SEE >FO; ',
-				'       **CARRIER MAY OFFER ADDITIONAL SERVICES**SEE >FQ/DASO;',
-				'    SRC65LGB BEST FARE FOR THIS PASSENGER                       ',
-				'    SRC65LGB RATE USED IN EQU TOTAL IS BSR 1EUR - 1.230293USD   ',
-				'    SRC65LGB LAST DATE TO PURCHASE TICKET: 03MAY18              ',
-				'    SRC65LGB TICKETING AGENCY 711M                              ',
-				'    SRC65LGB DEFAULT PLATING CARRIER PS                         ',
-				'    SRC65LGB E-TKT REQUIRED                                     ',
-				'    SRC75LGB BEST FARE FOR THIS PASSENGER                       ',
-				'    SRC75LGB RATE USED IN EQU TOTAL IS BSR 1EUR - 1.230293USD   ',
-				'    SRC75LGB LAST DATE TO PURCHASE TICKET: 03MAY18              ',
-				'    SRC75LGB TICKETING AGENCY 711M                              ',
-				'    SRC75LGB DEFAULT PLATING CARRIER PS                         ',
-				'    SRC75LGB E-TKT REQUIRED                                     ',
-				'BAGGAGE ALLOWANCE',
-				'SRC',
-				' PS KIVRIX  2PC  ',
-				'   BAG 1 -  NO FEE       UPTO70LB/32KG AND UPTO62LI/158LCM ',
-				'   BAG 2 -  NO FEE       UPTO70LB/32KG AND UPTO62LI/158LCM ',
-				'   MYTRIPANDMORE.COM/BAGGAGEDETAILSPS.BAGG             ',
-				'                                                               ',
-				'CARRY ON ALLOWANCE',
-				' PS KIVIEV  12K   ',
-				'   BAG 1 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'   BAG 2 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				' PS IEVRIX  12K   ',
-				'   BAG 1 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'   BAG 2 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'SRC',
-				' PS KIVRIX  2PC  ',
-				'   BAG 1 -  NO FEE       UPTO70LB/32KG AND UPTO62LI/158LCM ',
-				'   BAG 2 -  NO FEE       UPTO70LB/32KG AND UPTO62LI/158LCM ',
-				'   MYTRIPANDMORE.COM/BAGGAGEDETAILSPS.BAGG             ',
-				'                                                               ',
-				'CARRY ON ALLOWANCE',
-				' PS KIVIEV  12K   ',
-				'   BAG 1 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'   BAG 2 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				' PS IEVRIX  12K   ',
-				'   BAG 1 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'   BAG 2 -  CHGS MAY APPLY IF BAGS EXCEED TTL WT ALLOWANCE',
-				'                                                               ',
-				'BAGGAGE DISCOUNTS MAY APPLY BASED ON FREQUENT FLYER STATUS/',
-				'ONLINE CHECKIN/FORM OF PAYMENT/MILITARY/ETC.',
-			]),
-			{
-				'ptcRows': [
-					{'passengerNumbers': [1], 'ptc': 'ADT'},
-					{'passengerNumbers': [2], 'ptc': 'ADT'},
-				],
-				'ptcMessages': [
-					{'ptc': 'SRC', 'ptcDescription': '65LGB', 'raw': 'BEST FARE FOR THIS PASSENGER                       '},
-					{'ptc': 'SRC', 'ptcDescription': '65LGB', 'raw': 'RATE USED IN EQU TOTAL IS BSR 1EUR - 1.230293USD   '},
-					{'ptc': 'SRC', 'ptcDescription': '65LGB', 'raw': 'LAST DATE TO PURCHASE TICKET: 03MAY18              '},
-					{'ptc': 'SRC', 'ptcDescription': '65LGB', 'raw': 'TICKETING AGENCY 711M                              '},
-					{'ptc': 'SRC', 'ptcDescription': '65LGB', 'raw': 'DEFAULT PLATING CARRIER PS                         '},
-					{'ptc': 'SRC', 'ptcDescription': '65LGB', 'raw': 'E-TKT REQUIRED                                     '},
-					{'ptc': 'SRC', 'ptcDescription': '75LGB', 'raw': 'BEST FARE FOR THIS PASSENGER                       '},
-					{'ptc': 'SRC', 'ptcDescription': '75LGB', 'raw': 'RATE USED IN EQU TOTAL IS BSR 1EUR - 1.230293USD   '},
-					{'ptc': 'SRC', 'ptcDescription': '75LGB', 'raw': 'LAST DATE TO PURCHASE TICKET: 03MAY18              '},
-					{'ptc': 'SRC', 'ptcDescription': '75LGB', 'raw': 'TICKETING AGENCY 711M                              '},
-					{'ptc': 'SRC', 'ptcDescription': '75LGB', 'raw': 'DEFAULT PLATING CARRIER PS                         '},
-					{'ptc': 'SRC', 'ptcDescription': '75LGB', 'raw': 'E-TKT REQUIRED                                     '},
-				],
-			},
-		]);
-
-		// PRIVATE FARE SELECTED example
-		// > 0 AT  262 D  20SEP NBOCMN NN1;
-		// > 0 AT  202 D  20SEP CMNJFK NN1;
-		// > 0 AT  201 D  29SEP JFKCMN NN1;
-		// > 0 AT  263 D  30SEP CMNNBO NN1;
-		$list.push([
-			php.implode(php.PHP_EOL, [
-				'>FQ',
-				'   PSGR                  FARE     TAXES         TOTAL PSG DES   ',
-				'FQP 1         USD     3236.00     527.41      3763.41 ADT       ',
-				'GRAND TOTAL INCLUDING TAXES ****     USD      3763.41           ',
-				'             **ADDITIONAL FEES MAY APPLY**SEE >FO; ',
-				'       **CARRIER MAY OFFER ADDITIONAL SERVICES**SEE >FQ/DASO;',
-				'    ADT      PRIVATE FARE SELECTED                              ',
-				'    ADT      CAT35                                              ',
-				'    ADT      TOUR CODE: AT6COM                                  ',
-				'    ADT      LAST DATE TO PURCHASE TICKET: 20SEP18              ',
-				'    ADT      TICKETING AGENCY 711M                              ',
-				'    ADT      DEFAULT PLATING CARRIER AT                         ',
-				'    ADT      E-TKT REQUIRED                                     ',
-				'UNABLE TO FILE - NEED NAMES',
-				'BAGGAGE ALLOWANCE',
-				'ADT',
-				' AT NBONYC  3PC  ',
-				'   BAG 1 -  NO FEE       UPTO50LB/23KG                     ',
-				'   BAG 2 -  NO FEE       UPTO50LB/23KG                     ',
-				'   MYTRIPANDMORE.COM/BAGGAGEDETAILSAT.BAGG             ',
-				'                                                               ',
-				' AT NYCNBO  3PC  ',
-				'   BAG 1 -  NO FEE       UPTO50LB/23KG                     ',
-				'   BAG 2 -  NO FEE       UPTO50LB/23KG                     ',
-				'   MYTRIPANDMORE.COM/BAGGAGEDETAILSAT.BAGG             ',
-				'                                                               ',
-				'CARRY ON ALLOWANCE',
-				' AT NBOCAS  1PC   ',
-				'   BAG 1 -  NO FEE       UPTO22LB/10KG AND UPTO45LI/115LCM ',
-				' AT CASNYC  1PC   ',
-				'   BAG 1 -  NO FEE       UPTO22LB/10KG AND UPTO45LI/115LCM ',
-				' AT NYCCAS  1PC   ',
-				'   BAG 1 -  NO FEE       UPTO22LB/10KG AND UPTO45LI/115LCM ',
-				' AT CASNBO  1PC   ',
-				'   BAG 1 -  NO FEE       UPTO22LB/10KG AND UPTO45LI/115LCM ',
-				'                                                               ',
-				'BAGGAGE DISCOUNTS MAY APPLY BASED ON FREQUENT FLYER STATUS/',
-				'ONLINE CHECKIN/FORM OF PAYMENT/MILITARY/ETC.',
-			]),
-			{
-				'ptcRows': [
-					{
-						'guaranteeCode': 'P',
-						'passengerNumbers': [1],
-						'baseFare': '3236.00',
-						'netPrice': '3763.41',
-					},
-				],
-				'ptcMessages': [
-					{'ptc': 'ADT', 'type': 'hasPrivateFaresSelectedMessage', 'parsed': true},
-				],
-			},
-		]);
-
-		// another PRIVATE FARE example
-		$list.push([
-			php.implode(php.PHP_EOL, [
-				">FQBB/-TPACK/:P/TA0GF/||-AB/*JWZ",
-				"                   *** BEST BUY QUOTATION ***",
-				"             LOWEST FARE AVAILABLE FOR CABIN BOOKED",
-				"              *** REBOOK BF SEGMENTS 2V/3V/4V ***",
-				"   PSGR   QUOTE BASIS         FARE    TAXES      TOTAL PSG DES  ",
-				"FQZ 1     OLN4Z1M4|   GBP   450.00   271.52     721.52 ADT      ",
-				"GRAND TOTAL INCLUDING TAXES ****     GBP       721.52           ",
-				"             **ADDITIONAL FEES MAY APPLY**SEE >FO;              ",
-				"    ADT      PRIVATE FARE SELECTED                              ",
-				"    ADT      CAT35                                              ",
-				"    ADT      SUM IDENTIFIED AS UB IS A PASSENGER SERVICE CHARGE ",
-				"    ADT      TOUR CODE: GBSO                                    ",
-				"    ADT      FARE HAS FORM OF PAYMENT RESTRICTION               ",
-				"    ADT      LAST DATE TO PURCHASE TICKET: 02SEP19 LON          ",
-				"    ADT      TICKETING AGENCY 0GF                               ",
-				"    ADT      DEFAULT PLATING CARRIER AA                         ",
-				"    ADT      FARE HAS A PLATING CARRIER RESTRICTION             ",
-				"    ADT      E-TKT REQUIRED                                     ",
-				"UNABLE TO FILE - NEED NAMES",
-				"BAGGAGE ALLOWANCE",
-				"ADT",
-				" AA LONABQ  1PC  ",
-				"   BAG 1 -  NO FEE       UPTO50LB/23KG AND UPTO62LI/158LCM ",
-				"   BAG 2 -  65.00 GBP    UPTO50LB/23KG AND UPTO62LI/158LCM ",
-				"   VIEWTRIP.TRAVELPORT.COM/BAGGAGEPOLICY/AA            ",
-				"                                                               ",
-				" AA ABQLON  1PC  ",
-				"   BAG 1 -  NO FEE       UPTO50LB/23KG AND UPTO62LI/158LCM ",
-				"   BAG 2 -  65.00 GBP    UPTO50LB/23KG AND UPTO62LI/158LCM ",
-				"   VIEWTRIP.TRAVELPORT.COM/BAGGAGEPOLICY/AA            ",
-				"                                                               ",
-				"CARRY ON ALLOWANCE",
-				" AA LONLAX  2PC   ",
-				"   BAG 1 -  NO FEE       UPTO45LI/115LCM                   ",
-				"   BAG 2 -  NO FEE       CARRYON HAND BAGGAGE ALLOWANCE    ",
-				" AA LAXABQ  2PC   ",
-				"   BAG 1 -  NO FEE       UPTO45LI/115LCM                   ",
-				"   BAG 2 -  NO FEE       CARRYON HAND BAGGAGE ALLOWANCE    ",
-				" AA ABQDFW  2PC   ",
-				"   BAG 1 -  NO FEE       UPTO45LI/115LCM                   ",
-				"   BAG 2 -  NO FEE       CARRYON HAND BAGGAGE ALLOWANCE    ",
-				" AA DFWLON  2PC   ",
-				"   BAG 1 -  NO FEE       UPTO45LI/115LCM                   ",
-				"   BAG 2 -  NO FEE       CARRYON HAND BAGGAGE ALLOWANCE    ",
-				"                                                               ",
-				"EMBARGO - FOR BAGGAGE LIMITATIONS SEE -",
-				" AA LONLAX      VIEWTRIP.TRAVELPORT.COM/BAGGAGEPOLICY/AA",
-				" AA LAXABQ      VIEWTRIP.TRAVELPORT.COM/BAGGAGEPOLICY/AA",
-				" AA ABQDFW      VIEWTRIP.TRAVELPORT.COM/BAGGAGEPOLICY/AA",
-				" AA DFWLON      VIEWTRIP.TRAVELPORT.COM/BAGGAGEPOLICY/AA",
-				"                                                               ",
-				"BAGGAGE DISCOUNTS MAY APPLY BASED ON FREQUENT FLYER STATUS/",
-				"ONLINE CHECKIN/FORM OF PAYMENT/MILITARY/ETC.",
-				"",
-			]),
-			{
-				'ptcMessages': [
-					{'ptc': 'ADT', 'type': 'hasPrivateFaresSelectedMessage', 'parsed': true},
-				],
-			},
-		]);
-
-		$list.push([
-			php.implode(php.PHP_EOL, [
-				'>FQBBK',
-				'                   ***  BEST BUY REBOOK  ***',
-				'                  ***  REBOOK SUCCESSFUL  ***',
-				'                    REBOOKED SEGMENTS 1B/2Y',
-				'    PSGR                      FARE     TAXES     TOTAL PSG DES  FQG 1                USD   2583.00    206.50   2789.50 ADT          GUARANTEED AT TIME OF TICKETING',
-				'GRAND TOTAL INCLUDING TAXES  ****      USD     2789.50                       **ADDITIONAL FEES MAY APPLY**SEE >FO;              ><',
-			]),
-			{
-				'cmdCopy': 'FQBBK',
-				'rebookStatus': 'successful',
-				'headerMessages': ['                   ***  BEST BUY REBOOK  ***','                    REBOOKED SEGMENTS 1B/2Y'],
-				'ptcRows': [
-					{
-						'guaranteeCode': 'G',
-						'passengerNumbers': [1],
-						'currency': 'USD',
-						'baseFare': '2583.00',
-						'taxAmount': '206.50',
-						'netPrice': '2789.50',
-						'ptc': 'ADT',
-					},
-				],
-				'grandTotal': {'currency': 'USD','amount': '2789.50'},
-			},
-		]);
-
-		return $list;
+		return list;
 	}
 
 	/**
