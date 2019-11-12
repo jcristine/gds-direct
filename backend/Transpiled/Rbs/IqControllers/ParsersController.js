55c250e557699bb5b76e7ef3cc67456c243ab3ef
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-11-12 21:09:09 +0000

    Choose best guessDumpType() match by amount of parsed passengers and segments

--- backend/Transpiled/Rbs/IqControllers/ParsersController.js
+++ backend/Transpiled/Rbs/IqControllers/ParsersController.js
@@ -16,57 +16,86 @@ const PNR_DUMP_TYPES = [
 	'amadeus_pnr',
 ];
 
+const compareResults = (aArg, bArg) => {
+	const norm = result => ({
+		paxCnt: result.data.passengers.length,
+		segCnt: result.data.itinerary.length,
+	});
+	const a = norm(aArg);
+	const b = norm(bArg);
+
+		// one of them lacks segments
+	return +(a.segCnt && !b.segCnt)
+		|| -(b.segCnt && !a.segCnt)
+		// one of them lacks paxes
+		|| +(a.paxCnt && !b.paxCnt)
+		|| -(b.paxCnt && !a.paxCnt)
+		// one of them has more segments
+		|| +(a.segCnt - b.segCnt)
+		|| -(b.segCnt - a.segCnt)
+		// one of them has more paxes
+		|| +(a.paxCnt - b.paxCnt)
+		|| -(b.paxCnt - a.paxCnt);
+};
+
 class ParsersController {
-	tryParseAs($dump, $baseDate) {
-		const asApollo = ApoPnrParser.parse($dump);
-		const asGalileo = GalPnrParser.parse($dump);
-		const asSabre = SabPnrParser.parse($dump);
-		const asAmadeus = AmaPnrParser.parse($dump);
+	tryParseAs(dump, baseDate) {
+		const asApollo = ApoPnrParser.parse(dump);
+		const asGalileo = GalPnrParser.parse(dump);
+		const asSabre = SabPnrParser.parse(dump);
+		const asAmadeus = AmaPnrParser.parse(dump);
 		// make sure first line trimmed space is restored,
 		// since Amadeus parser is strict about indentation
-		const normAma = $dump.replace(/^\s*/, '  ');
+		const normAma = dump.replace(/^\s*/, '  ');
 		const asAmaItin = AmaPnrParser.parse(normAma);
 
+		const options = [];
+
 		if (asApollo.itineraryData.length > 0) {
-			const data = FormatAdapter.adaptApolloPnrParseForClient(asApollo, $baseDate);
-			return {type: 'apollo_pnr', data: data};
-		} else if (
+			const data = FormatAdapter.adaptApolloPnrParseForClient(asApollo, baseDate);
+			options.push({type: 'apollo_pnr', data: data});
+		}
+		if (
 			asGalileo.itineraryData.length > 0 ||
 			asGalileo.passengers.passengerList.length > 0
 		) {
-			const data = GalileoPnrCommonFormatAdapter.transform(asGalileo, $baseDate);
-			return {type: 'galileo_pnr', data: data};
-		} else if (
+			const data = GalileoPnrCommonFormatAdapter.transform(asGalileo, baseDate);
+			options.push({type: 'galileo_pnr', data: data});
+		}
+		if (
 			asAmadeus.parsed.itinerary.length > 0 &&
 			asAmadeus.parsed.itinerary.length >= asAmaItin.parsed.itinerary.length ||
 			asAmadeus.parsed.passengers.length > 0 ||
 			asAmadeus.parsed.pnrInfo.recordLocator
 		) {
-			const data = AmadeusPnrCommonFormatAdapter.transform(asAmadeus, $baseDate);
-			return {type: 'amadeus_pnr', data: data};
-		} else if (asAmaItin.parsed.itinerary.length > 0) {
-			const data = AmadeusPnrCommonFormatAdapter.transform(asAmaItin, $baseDate);
-			return {type: 'amadeus_pnr', data: data};
-		} else if (
+			const data = AmadeusPnrCommonFormatAdapter.transform(asAmadeus, baseDate);
+			options.push({type: 'amadeus_pnr', data: data});
+		}
+		if (asAmaItin.parsed.itinerary.length > 0) {
+			const data = AmadeusPnrCommonFormatAdapter.transform(asAmaItin, baseDate);
+			options.push({type: 'amadeus_pnr', data: data});
+		}
+		if (
 			asSabre.parsedData.itinerary.length > 0 ||
 			(asSabre.parsedData.pnrInfo || {}).pcc
 		) {
-			const data = FormatAdapter.adaptSabrePnrParseForClient(asSabre, $baseDate);
-			return {type: 'sabre_pnr', data: data};
-		// no itinerary (ambiguous) dumps follow, usually pax list - treat as apollo
-		} else if (asApollo.passengers.passengerList.length > 0) {
-			const data = FormatAdapter.adaptApolloPnrParseForClient(asApollo, $baseDate);
-			return {type: 'apollo_pnr', data: data};
-		} else {
-			return null;
+			const data = FormatAdapter.adaptSabrePnrParseForClient(asSabre, baseDate);
+			options.push({type: 'sabre_pnr', data: data});
 		}
+		// no itinerary (ambiguous) dumps follow, usually pax list - treat as apollo
+		if (asApollo.passengers.passengerList.length > 0) {
+			const data = FormatAdapter.adaptApolloPnrParseForClient(asApollo, baseDate);
+			options.push({type: 'apollo_pnr', data: data});
+		}
+		return options;
 	}
 
 	guessDumpType(params) {
 		const dump = php.strtoupper(params.dump);
-		const result = this.tryParseAs(dump, params.creationDate || null);
-		return result
-			? {response_code: 1, result: result, errors: []}
+		const bestResult = this.tryParseAs(dump, params.creationDate || null)
+			.reverse().sort(compareResults).slice(-1)[0];
+		return bestResult
+			? {response_code: 1, result: bestResult, errors: []}
 			: {response_code: 3, result: {type: 'unknown', data: null}, errors: []};
 	}
 }
